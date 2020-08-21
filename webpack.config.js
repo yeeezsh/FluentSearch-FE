222c7c3cbebfbd1087e925642b2d4b1e86f5dcea
Korbboon <korbboon.thuswongsa@agoda.com>
2020-08-21 20:56:11 +0700

    Add alias path

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,18 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      Apps: path.resolve(__dirname, 'src/apps'),
+      Assets: path.resolve(__dirname, 'src/assets'),
+      Components: path.resolve(__dirname, 'src/components'),
+      Hooks: path.resolve(__dirname, 'src/hooks'),
+      Models: path.resolve(__dirname, 'src/models'),
+      Pages: path.resolve(__dirname, 'src/pages'),
+      Routers: path.resolve(__dirname, 'src/routers'),
+      Services: path.resolve(__dirname, 'src/services'),
+      Stores: path.resolve(__dirname, 'src/stores'),
+      Utils: path.resolve(__dirname, 'src/utils'),
+    },
   },
   output: {
     path: path.join(__dirname, '/dist'),
