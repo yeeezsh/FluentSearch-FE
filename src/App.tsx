1688b04bcfe19a98ae95ae1468e6b168a0762fe3
Korbboon <korbboon.thuswongsa@agoda.com>
2020-08-23 15:23:21 +0700

    Fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="App">
       <header className="App-header">
