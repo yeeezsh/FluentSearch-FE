9c90b76e14e1f1c4dd6e7f4a7e7e6d06a97df391
octobrekids <chnrd.k@gmail.com>
2020-08-23 21:05:13 +0700

    Use pathJoin for menu link

--- src/components/Layouts/Home/HomeNavbar.tsx
+++ src/components/Layouts/Home/HomeNavbar.tsx
@@ -1,5 +1,6 @@
 import menu from 'Models/menu/app.navbar.constant';
 import React from 'react';
+import pathJoin from 'Utils/path-join';
 import style from './styles.module.css';
 
 const HomeNavbar: React.FC = () => {
@@ -9,7 +10,7 @@ const HomeNavbar: React.FC = () => {
         {menu.map(({ key, label, link }) => {
           return (
             <li key={key}>
-              <a href={link}>{label}</a>
+              <a href={pathJoin([link])}>{label}</a>
             </li>
           );
         })}
