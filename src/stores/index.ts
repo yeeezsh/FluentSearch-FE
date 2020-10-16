a2aa68e5f3b9a149316385f78744ed5b54ef9f11
octobrekids <chnrd.k@gmail.com>
2020-10-17 04:09:42 +0700

    Setup config redux-thunk as middleware

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,17 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import userReducer from './userRedcuer/userReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
+const middleware = [thunk];
+
 export type StoresState = ReturnType<typeof rootReducer>;
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+  reducer: rootReducer,
+  middleware,
+});
 
 export default store;
