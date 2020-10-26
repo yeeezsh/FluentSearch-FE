9376030acb59ff76b13ea359ebb6de414bd4faab
octobrekids <chnrd.k@gmail.com>
2020-10-26 16:40:05 +0700

    Edit Layout using antd

--- src/components/Layouts/Home/HomeLayout.tsx
+++ src/components/Layouts/Home/HomeLayout.tsx
@@ -1,12 +1,17 @@
+import { Layout } from 'antd';
 import React from 'react';
 import HomeNavbar from './HomeNavbar';
 
 const HomeLayout: React.FC = (props) => {
   const { children } = props;
+  const { Content, Footer } = Layout;
   return (
     <div>
       <HomeNavbar />
-      <div>{children}</div>
+      <Content>
+        <div>{children}</div>
+      </Content>
+      <Footer>Footer</Footer>
     </div>
   );
 };
