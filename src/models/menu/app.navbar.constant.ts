9a8f388bd9896191a5e9936f8d2a59d26130235a
octobrekids <chnrd.k@gmail.com>
2020-10-28 02:44:18 +0700

    Remove unused navbar menu

--- src/models/menu/app.navbar.constant.ts
+++ src/models/menu/app.navbar.constant.ts
@@ -3,17 +3,12 @@ import MenuType from './menu.type';
 const APP_NAVBAR_CONSTANT: MenuType[] = [
   {
     key: '1',
-    label: 'Login',
-    link: ['asdasd'],
-  },
-  {
-    key: '2',
     label: 'Sign up',
     link: ['asdasd'],
   },
   {
-    key: '3',
-    label: 'sdfsdf',
+    key: '2',
+    label: 'Login',
     link: ['asdasd'],
   },
 ];
