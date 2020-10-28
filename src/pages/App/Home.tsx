1bcf1b59cf6e98c16beb23db442a30af11947350
octobrekids <chnrd.k@gmail.com>
2020-10-29 02:51:36 +0700

    Add Header Image at Home Page

--- src/pages/App/Home.tsx
+++ src/pages/App/Home.tsx
@@ -1,8 +1,17 @@
 import HomeLayout from 'Components/Layouts/Home/HomeLayout';
 import React from 'react';
+import style from './styles.module.css';
 
 const Home: React.FC = () => {
-  return <HomeLayout>test</HomeLayout>;
+  return (
+    <HomeLayout>
+      <div className={style.bg} />
+      <div>
+        <h1>Heading</h1>
+        <p>Lorem ipsum..</p>
+      </div>
+    </HomeLayout>
+  );
 };
 
 export default Home;
