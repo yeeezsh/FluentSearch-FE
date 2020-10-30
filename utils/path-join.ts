04e40aabc616483c0b53585c995b49332c0c7dba
octobrekids <chnrd.k@gmail.com>
2020-10-31 02:44:14 +0700

    Fix anonymous function pathJoin

--- utils/path-join.ts
+++ utils/path-join.ts
@@ -1,5 +1,6 @@
-export default (paths: string[], sep?: string): string => {
+const pathJoin = (paths: string[], sep?: string): string => {
   const defaultSeperator = '/';
   if (paths.length === 1) return `${paths[0]}${sep || defaultSeperator}`;
   return paths.join(sep || defaultSeperator);
 };
+export default pathJoin;
