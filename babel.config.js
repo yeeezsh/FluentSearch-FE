d2664b4a7e7d0d4c849615a07f7337bbdeaba514
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-31 00:02:07 +0700

    Add antd

--- babel.config.js
+++ babel.config.js
@@ -7,4 +7,17 @@ module.exports = {
       presets: ['next/babel'],
     },
   },
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+      'import-antd',
+    ],
+  ],
 };
