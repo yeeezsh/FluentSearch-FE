21610617a21b95290e287298e2dad65e24c8f014
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-30 22:00:07 +0700

    Migrate stores

---
+++ stores/index.spec.ts
@@ -0,0 +1,19 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { middleware } from '.';
+
+const mockStore = configureStore(middleware);
+
+describe('stores test', () => {
+  const initialState = {};
+  const store = mockStore(initialState);
+  it('it should correctly define initial state', () => {
+    const expectedInitial = {};
+    expect(store.getState()).toEqual(expectedInitial);
+  });
+
+  it('it should defined correctly middleware', () => {
+    const expectedMiddleware = [thunk];
+    expect(middleware).toEqual(expectedMiddleware);
+  });
+});
