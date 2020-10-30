b7a6b1733c6aea717b044d2aad9aadd8d421ac12
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-30 23:25:35 +0700

    Run lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import { Provider } from 'react-redux'
-import { store } from 'Stores/stores'
-import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import { store } from 'Stores/stores';
+import '../styles/globals.css';
 
-export function MyApp({ Component, pageProps }) {
+export function MyApp(Component, pageProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
