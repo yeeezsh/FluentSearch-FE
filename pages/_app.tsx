cb56622eb801562c3ab74994679783a01aad2314
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-30 23:32:37 +0700

    Fix typing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { Provider } from 'react-redux';
 import { store } from 'Stores/stores';
 import '../styles/globals.css';
 
-export function MyApp(Component, pageProps) {
+export function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
