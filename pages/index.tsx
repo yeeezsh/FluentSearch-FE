c86f03aa66c8b033140218493cd8d95ed50cc2c2
octobrekids <chnrd.k@gmail.com>
2020-11-02 23:13:27 +0700

    Add card at home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Card, Col, Row } from 'antd';
 import Button from 'Components/Button/Button';
 import HomeLayout from 'Components/Layouts/Home/HomeLayout';
 import React from 'react';
@@ -9,9 +10,31 @@ export default function Home(): JSX.Element {
         <div className={style.headerText}>
           <h1>Heading</h1>
           <p>Lorem ipsum..</p>
-          <Button type="Danger">CANCEL</Button>
+          <Button type="Danger">Explore</Button>
         </div>
       </div>
+      <Row gutter={[0, 24]} justify="space-around" style={{ marginTop: '20px' }}>
+        <Col span={6}>
+          <Card> Feature 1 </Card>
+        </Col>
+        <Col span={6}>
+          <Card> Feature 1 </Card>
+        </Col>
+        <Col span={6}>
+          <Card> Feature 1 </Card>
+        </Col>
+      </Row>
+      <Row gutter={[0, 24]} justify="space-around">
+        <Col span={6}>
+          <Card> Feature 1 </Card>
+        </Col>
+        <Col span={6}>
+          <Card> Feature 1 </Card>
+        </Col>
+        <Col span={6}>
+          <Card> Feature 1 </Card>
+        </Col>
+      </Row>
     </HomeLayout>
   );
 }
