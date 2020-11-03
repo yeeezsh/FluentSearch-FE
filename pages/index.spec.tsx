6f7da9b104a1e5ec6d927165fcc471950df1c15b
Korbboon <korbboon.thuswongsa@agoda.com>
2020-11-03 16:15:34 +0700

    Add home page specs

---
+++ pages/index.spec.tsx
@@ -0,0 +1,10 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Home from '.';
+
+describe('Home', () => {
+  it('Should render correctly', () => {
+    const wrapper = shallow(<Home />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
