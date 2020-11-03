30bc39b7c72e640a3e17b8ce8eea6ec2f4a39ca2
Korbboon <korbboon.thuswongsa@agoda.com>
2020-11-03 23:29:55 +0700

    ignore test on out & build dir

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/out/',
+    '<rootDir>/build/',
+  ],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
