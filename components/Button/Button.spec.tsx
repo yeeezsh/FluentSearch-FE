528e2ae239ebdf175c8c82ded496ae98bba6e9f9
yee2542 <yee2542gtl@gmail.com>
2020-11-04 16:50:36 +0700

    Format

--- components/Button/Button.spec.tsx
+++ components/Button/Button.spec.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Button from './Button';
 
 describe('Button Components', () => {
-    it('Button should exisiting', () => {
-        const wrapper = shallow(<Button/>)
-        expect(wrapper.exists()).toBe(true)
-    })
-})
+  it('Button should exisiting', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
