89d02705ee86a6323194b2ee3df99ab5dade159c
yee2542 <yee2542gtl@gmail.com>
2020-11-04 23:58:48 +0700

    Try to fix jest effort 555555555

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleDirectories: ['./', 'node_modules'],
   modulePaths: ['<rootDir>'],
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
