1af743149e0180293ae37fd436a71445d3cd0f99
yee2542 <yee2542gtl@gmail.com>
2020-11-04 16:37:42 +0700

    remove unused file

--- pages/__test__/index.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import Home from 'Pages/index';
-
-describe('Home', () => {
-  it('Should render correctly', () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper.exists()).toBe(true);
-  });
-});
