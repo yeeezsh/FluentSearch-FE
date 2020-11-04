0a3191c3ae5b5c3a2e3f59912a27bb0b9d6d08bb
yee2542 <yee2542gtl@gmail.com>
2020-11-04 21:51:53 +0700

    Try to fix jest effort 3

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  roots: ['<rootDir>'],
-  moduleDirectories: ['node_modules', './'],
+  roots: ['<rootDir>/'],
+  moduleDirectories: ['node_modules', '/'],
   modulePaths: ['<rootDir>'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -13,7 +13,7 @@ module.exports = {
   setupFiles: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
+    // '<rootDir>/node_modules/',
     '<rootDir>/out/',
     '<rootDir>/build/',
     '<rootDir>/assets/',
