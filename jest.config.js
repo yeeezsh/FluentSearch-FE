b56f8ecdeaf60741510c021d1416a9c6d3295b14
yee2542 <yee2542gtl@gmail.com>
2020-11-05 00:52:28 +0700

    kuy 2

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,16 @@
 module.exports = {
-  // testEnvironment: 'node',
-  // roots: ['<rootDir>/'],
-  // preset: 'ts-jest',
-  // moduleDirectories: ['./', 'node_modules'],
-  modulePaths: ['<rootDir>'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['./', 'node_modules'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
-  // transform: {
-  //   '^.+\\.(ts|tsx)$': 'ts-jest',
-  // },
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    // '<rootDir>/node_modules/',
-    '<rootDir>/out/',
-    '<rootDir>/build/',
-    '<rootDir>/assets/',
-    '<rootDir>/server/',
-    '<rootDir>/public/',
-    '<rootDir>/styles/',
-  ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.jest.json',
