5b21b2734515fec09796749acb9bddf34ea49ee9
yee2542 <yee2542gtl@gmail.com>
2020-11-05 16:21:49 +0700

    kuy 6

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,8 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
-    "^Components(.*)$": "<rootDir>/components$1",
+    // "^Components(.*)$": "<rootDir>/components/$1",
+    "^Components/(.*)": "<rootDir>/components/$1"
   },
 
   globals: {
