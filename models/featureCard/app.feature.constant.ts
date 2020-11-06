7503ca08f017bf353554ee01340fe9e48242d4e1
octobrekids <chnrd.k@gmail.com>
2020-11-06 18:37:23 +0700

    Add feature constant and type

---
+++ models/featureCard/app.feature.constant.ts
@@ -0,0 +1,46 @@
+import FeatureType from './feature.type';
+const APP_FEATURE_CONSTANT: FeatureType[] = [
+  {
+    key: '1',
+    icon: '<CarOutlined />',
+    label: 'Feature 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum iusto',
+  },
+  {
+    key: '2',
+    icon: 'CarOutlined',
+    label: 'Feature 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum iusto',
+  },
+  {
+    key: '3',
+    icon: 'CarOutlined',
+    label: 'Feature 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum iusto',
+  },
+  {
+    key: '4',
+    icon: 'CarOutlined',
+    label: 'Feature 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum iusto',
+  },
+  {
+    key: '5',
+    icon: 'CarOutlined',
+    label: 'Feature 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum iusto',
+  },
+  {
+    key: '6',
+    icon: 'CarOutlined',
+    label: 'Feature 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum iusto',
+  },
+];
+export default APP_FEATURE_CONSTANT;
---
+++ models/featureCard/feature.type.ts
@@ -0,0 +1,7 @@
+type FeatureType = {
+  key: string;
+  icon: string;
+  label: string;
+  description: string;
+};
+export default FeatureType;
