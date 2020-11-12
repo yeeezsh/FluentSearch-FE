1fc73f2f7ee7566357888d92cf32ad5c4699c7c7
octobrekids <chnrd.k@gmail.com>
2020-11-12 21:16:40 +0700

    Fix missing type import

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,7 +1,8 @@
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta, Story } from '@storybook/react/types-6-0';
+import CustomButton from 'Components/Button/CustomButton';
+import { ButtonComponentProps } from 'Models/button/button.type';
 import React from 'react';
-import CustomButton, { ButtonComponentProps } from '../components/Button/CustomButton';
 
 export default {
   title: 'DeepFlow/Button',
