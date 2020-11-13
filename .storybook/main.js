ba9ad23b246c7f44e0343e73a52af19bdd00b097
yee2542 <yee2542gtl@gmail.com>
2020-11-13 22:31:44 +0700

    Remove unused args

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()];
     return config;
   },
