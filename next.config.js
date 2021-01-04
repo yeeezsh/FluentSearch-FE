45c333f71f35b37366d325fcaa14ea5b4507874d
yee2542 <yee2542gtl@gmail.com>
2021-01-04 21:40:13 +0700

    Fix bad images

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [],
     domains: ['fluentsearch.com'],
-    path: '/_next/image',
-    loader: 'default',
+    path: '/public/images',
+    loader: 'imgix',
   },
 };
