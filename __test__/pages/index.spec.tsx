4ad8d9749cf91589bbd456ccb4236d25abcf7b0e
octobrekids <chnrd.k@gmail.com>
2021-01-05 16:14:32 +0700

    Fix merge conflict

--- __test__/pages/index.spec.tsx
+++ __test__/pages/index.spec.tsx
@@ -1,10 +1,12 @@
 import HomeLayout from 'Components/Layouts/Home/HomeLayout';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import Home from 'Pages/index';
 import React from 'react';
 
 describe('Pages/Home test', () => {
-  it('Render correcly', () => {
+  const wrap = shallow(<Home />);
+  console.log(wrap.debug());
+  it('Render correctly', () => {
     const wrap = <Home />;
     expect(wrap).toMatchSnapshot();
   });
@@ -14,8 +16,8 @@ describe('Pages/Home test', () => {
     expect(wrap.exists()).toBe(true);
   });
 
-  it('Should contain Home Layout', () => {
-    const wrap = mount(<Home />);
+  it('Should contain HomeLayout', () => {
+    const wrap = shallow(<Home />);
     expect(wrap.exists(HomeLayout)).toBe(true);
   });
 });
