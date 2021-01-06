a3fa44cc1168f8bde6ccfa7a2859d7948df2cc73
octobrekids <chnrd.k@gmail.com>
2021-01-06 19:54:14 +0700

    Add next router storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import { withNextRouter } from 'storybook-addon-next-router';
+import { addDecorator } from '@storybook/react';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+addDecorator(
+  withNextRouter({
+    path: '/', // defaults to `/`
+    asPath: '/', // defaults to `/`
+    query: {}, // defaults to `{}`
+    push() {} // defaults to using addon actions integration, can override any method in the router
+  })
+);
