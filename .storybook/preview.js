e689385f5c2fcf30013ed3bcda42581a0245fbe0
yee2542 <yee2542gtl@gmail.com>
2021-01-07 06:26:14 +0700

    WIP setup next/images for sb

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,23 @@
 import { withNextRouter } from 'storybook-addon-next-router';
 import { addDecorator } from '@storybook/react';
+import * as nextImage from 'next/image';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
 
 addDecorator(
   withNextRouter({
     path: '/', // defaults to `/`
     asPath: '/', // defaults to `/`
     query: {}, // defaults to `{}`
-    push() {} // defaults to using addon actions integration, can override any method in the router
-  })
+    push() {}, // defaults to using addon actions integration, can override any method in the router
+  }),
 );
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => {
+    return <img {...props} />;
+  },
+});
