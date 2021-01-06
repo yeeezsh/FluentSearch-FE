fb14b8df4750eef341fd8b9c8c5634fc9022fa54
octobrekids <chnrd.k@gmail.com>
2021-01-07 04:38:31 +0700

    Change HomeLayout test type

--- components/Layouts/Home/HomeLayout.spec.tsx
+++ components/Layouts/Home/HomeLayout.spec.tsx
@@ -1,5 +1,5 @@
 import HomeLayout from 'Components/Layouts/Home';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import { Layout } from 'antd';
 
@@ -17,12 +17,12 @@ describe('Components/HomeLayout test', () => {
   });
 
   it('Should contain Content', () => {
-    const wrap = mount(<HomeLayout />);
+    const wrap = shallow(<HomeLayout />);
     expect(wrap.exists(Content)).toBe(true);
   });
 
   it('Should contain Footer', () => {
-    const wrap = mount(<HomeLayout />);
+    const wrap = shallow(<HomeLayout />);
     expect(wrap.exists(Footer)).toBe(true);
   });
 });
