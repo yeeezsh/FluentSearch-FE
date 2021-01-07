36ab7095599d8e2bf3962279f470114783da8df7
octobrekids <chnrd.k@gmail.com>
2021-01-07 14:33:09 +0700

    Fix pathname

--- components/Layouts/FormCenter/index.tsx
+++ components/Layouts/FormCenter/index.tsx
@@ -1,5 +1,5 @@
 import { Col, Row } from 'antd';
-import { Container, FormContainer } from 'Components/Layouts/Login/styled';
+import { Container, FormContainer } from 'Components/Layouts/FormCenter/styled';
 import Image from 'next/image';
 import React from 'react';
 
