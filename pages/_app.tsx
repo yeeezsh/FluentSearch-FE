b23238e89ee3b6f94e0d8b024610f8a6559945c6
yee2542 <yee2542gtl@gmail.com>
2021-01-07 07:29:27 +0700

    Add exclude navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
 import 'antd/dist/antd.css';
 import HomeNavbar from 'Components/Layouts/Home/HomeNavbar';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from 'Stores/stores';
 import { GlobalStyle } from 'Styles/Global.styled';
 
+const EXCLUDE_NAVBAR: string[] = ['/login'];
+
 export function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const pathname = useRouter().pathname;
+  const isExcludeNavbar = EXCLUDE_NAVBAR.includes(pathname);
+
   return (
     <>
       <GlobalStyle />
       <Provider store={store}>
-        <HomeNavbar />
+        {!isExcludeNavbar && <HomeNavbar />}
         <Component {...pageProps} />
       </Provider>
     </>
