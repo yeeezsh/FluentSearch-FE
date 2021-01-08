aaac4774e1dd6191e3d61d0f5a8829d08aee17a3
octobrekids <chnrd.k@gmail.com>
2021-01-09 01:13:42 +0700

    Remove unused code in login stoies

--- stories/LoginPage.stories.tsx
+++ stories/LoginPage.stories.tsx
@@ -1,11 +1,9 @@
-import { Meta, Story } from '@storybook/react/types-6-0';
+import { Meta } from '@storybook/react/types-6-0';
 import LoginPage from 'Modules/user/pages/login';
 
 export default {
-  title: 'FluentSearch/LoginPage',
+  title: 'FluentSearch/Pages/LoginPage',
   component: LoginPage,
 } as Meta;
 
-const Template: Story = (args) => <LoginPage {...args} />;
-
-export const LoginStories = Template.bind({});
+export const LoginStories = (): JSX.Element => <LoginPage />;
