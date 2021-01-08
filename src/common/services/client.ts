bde6a9e9efb280ba67bd9e49c0440c3c06ca29ea
yee2542 <yee2542gtl@gmail.com>
2021-01-09 02:17:37 +0700

    implement env endpoint

--- src/common/services/client.ts
+++ src/common/services/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: '',
+  uri: process.env.APP_GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
 });
