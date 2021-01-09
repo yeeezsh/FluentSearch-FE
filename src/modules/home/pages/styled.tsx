f2f39f8f7d243f1420941ad1f37973507a16c2ed
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-01-09 19:38:28 +0700

    Fix home layout (#33)
    * add headwrapper
    
    * remove verbose code
    
    * add canvas wrapper
    
    * implement new styled
    
    * remove inline
    
    * format

--- src/modules/home/pages/styled.tsx
+++ src/modules/home/pages/styled.tsx
@@ -8,15 +8,15 @@ export const gridStyle: CSSProperties = {
   textAlign: 'center',
 };
 
-export const HeaderWrapper = styled.div`
+export const CanvasWrapper = styled.div`
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+
   width: 100vw;
   height: 50vh;
-
   display: flex;
-  justify-content: center;
-  align-items: center;
-
-  text-align: center;
 
   background-image: url('https://pbs.twimg.com/media/ElVxJ8OWMAAnew5.jpg');
   background-size: cover;
@@ -25,9 +25,20 @@ export const HeaderWrapper = styled.div`
   background-attachment: fixed;
 `;
 
+export const HeaderWrapper = styled.div`
+  width: 100vw;
+  height: 28vh;
+`;
+
 export const Heading = styled.div`
+  flex-basis: 100%;
   font-size: 5em;
   color: #ffffff;
-  margin-top: 0;
-  margin-bottom: 0em;
+`;
+
+export const SubHeading = styled.div`
+  flex-basis: 100%;
+  font-size: 2em;
+  color: #ffffff;
+  margin-bottom: 28px;
 `;
