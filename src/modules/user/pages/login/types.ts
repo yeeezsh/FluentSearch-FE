988522b06dbdfd4cbb1ccb0cee9c4f249d92a449
octobrekids <chnrd.k@gmail.com>
2021-01-11 03:29:12 +0700

    add errorfield type

--- src/modules/user/pages/login/types.ts
+++ src/modules/user/pages/login/types.ts
@@ -1,7 +1,13 @@
 import { FormProps } from 'antd/lib/form/Form';
+import { InternalNamePath } from 'antd/lib/form/interface';
 
 export type FormLogin = { email: string; password: string };
-//TODO: define error fields
-export type FormLoginError = { values: FormLogin; errorFields: unknown[] };
+export type FormLoginError = {
+  values: FormLogin;
+  errorFields: {
+    name: InternalNamePath;
+    errors: string[];
+  }[];
+};
 export type FormErrorValue = FormProps['onFinishFailed'];
 export type FormFinishValue = FormProps['onFinish'];
