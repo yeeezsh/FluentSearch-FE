0e2fec1ce66980226fff51ae246e52dd7a6a9bdf
octobrekids <chnrd.k@gmail.com>
2021-01-13 04:52:03 +0700

    move styled loadedImage

--- src/modules/photos/components/LoadedImage/index.tsx
+++ src/modules/photos/components/LoadedImage/index.tsx
@@ -1,11 +1,5 @@
-import styled from 'styled-components';
 import { ImageProps } from '../Layouts/types';
-
-const Img = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
+import { Img } from './styled';
 
 export const LoadedImage = ({ url, key }: ImageProps): JSX.Element => {
   return <Img src={url} key={key} alt="" />;
