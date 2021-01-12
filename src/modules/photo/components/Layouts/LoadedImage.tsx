51b4789f09ef6814ead4d220364b3b1b62b44af8
octobrekids <chnrd.k@gmail.com>
2021-01-12 22:19:10 +0700

    remove unused type

--- src/modules/photo/components/Layouts/LoadedImage.tsx
+++ src/modules/photo/components/Layouts/LoadedImage.tsx
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
+import { ImageProps } from './types';
 
 const Img = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
 `;
-
-type ImageProps = {
-  url: string;
-  key: string;
-};
 
 export const LoadedImage = ({ url, key }: ImageProps): JSX.Element => {
   return <Img src={url} key={key} alt="" />;
