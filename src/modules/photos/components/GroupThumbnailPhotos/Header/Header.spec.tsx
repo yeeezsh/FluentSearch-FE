44320d44a489e776e2768084e8dbc370c5e8c5d2
yee2542 <yee2542gtl@gmail.com>
2021-01-13 05:21:07 +0700

    Refactor

--- src/modules/photos/components/GroupThumbnailPhotos/Header/Header.spec.tsx
+++ src/modules/photos/components/GroupThumbnailPhotos/Header/Header.spec.tsx
@@ -5,17 +5,18 @@ import Header from '.';
 import { DateHeaderStyled } from './styled';
 
 describe('GroupThumbnailPhotos / Header Components', () => {
+  const today = dayjs();
+
   it("Should show 'Today' when date is same date on props", () => {
-    const today = new Date();
-    const wrap = mount(<Header date={today} />);
+    const wrap = mount(<Header date={today.toDate()} />);
     const result = wrap.find(DateHeaderStyled).text();
     expect(result).toEqual('Today');
   });
 
   it('Should show Locale date string when date is different date on props', () => {
-    const today = dayjs().add(1, 'day');
-    const expected = today.format('ddd, MMM YYYY');
-    const wrap = mount(<Header date={today.toDate()} />);
+    const date = today.add(1, 'day');
+    const expected = date.format('ddd, MMM YYYY');
+    const wrap = mount(<Header date={date.toDate()} />);
     const result = wrap.find(DateHeaderStyled).text();
     expect(result).toEqual(expected);
   });
