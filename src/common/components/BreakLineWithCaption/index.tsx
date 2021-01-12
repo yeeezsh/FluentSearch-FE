2c45098270e93530de05b9343f229534532c8917
octobrekids <chnrd.k@gmail.com>
2021-01-12 23:50:19 +0700

    add InstanceOfstring

--- src/common/components/BreakLineWithCaption/index.tsx
+++ src/common/components/BreakLineWithCaption/index.tsx
@@ -3,14 +3,14 @@ import { Row, Col } from 'antd';
 import { P } from 'Styles/global';
 
 export const BreakLineWithCaption: React.FC = (props) => {
+  const isInstanceOfString = typeof props.children === 'string';
+
   return (
     <Row justify="center">
       <Col span={10}>
         <hr />
       </Col>
-      <Col span={4}>
-        <P>{props.children}</P>
-      </Col>
+      <Col span={4}>{isInstanceOfString ? <P>{props.children}</P> : props.children}</Col>
       <Col span={10}>
         <hr />
       </Col>
