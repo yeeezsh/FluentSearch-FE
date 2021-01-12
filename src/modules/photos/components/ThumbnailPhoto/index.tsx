1d34055e7a6503b777ca4789c48104799808ad33
yee2542 <yee2542gtl@gmail.com>
2021-01-13 03:26:41 +0700

    read src from props

--- src/modules/photos/components/ThumbnailPhoto/index.tsx
+++ src/modules/photos/components/ThumbnailPhoto/index.tsx
@@ -8,7 +8,7 @@ import {
 import { Props } from './types';
 
 const ThumbnailPhoto: React.FC<Props> = (props) => {
-  const { selected } = props;
+  const { selected, src } = props;
   const [hover, setHover] = useState<boolean>(selected);
 
   const onHover = (isHover: boolean) => {
@@ -26,7 +26,7 @@ const ThumbnailPhoto: React.FC<Props> = (props) => {
       <ThumbnailSelectIconWrapperLayoutStyled>
         <SelectIcon hover={hover} selected={selected} onSelect={onSelect} />
       </ThumbnailSelectIconWrapperLayoutStyled>
-      <ThumbnailPhotoDisplayStyled src="https://cdn.pixabay.com/photo/2015/07/31/14/59/beach-869195_1280.jpg" />
+      <ThumbnailPhotoDisplayStyled src={src} />
     </ThumbnailPhotoWrapperStyled>
   );
 };
