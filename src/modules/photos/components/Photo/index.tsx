ac139e53ffe6b1fbb237ed4f5b69f41def7ddd48
yee2542 <yee2542gtl@gmail.com>
2021-01-13 02:36:25 +0700

    Adjust Photo wrapper layout

--- src/modules/photos/components/Photo/index.tsx
+++ src/modules/photos/components/Photo/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SelectIcon from './SelectIcon';
-import { PhotoStyled } from './styled';
+import {
+  PhotoDisplayStyled,
+  PhotoWrapperStyled,
+  SelectIconWrapperLayoutStyled,
+} from './styled';
 import { Props } from './types';
 
 const Photo: React.FC<Props> = (props) => {
+  const { selected } = props;
+  const [hover, setHover] = useState<boolean>(selected);
+
+  const onHover = (isHover: boolean) => {
+    setHover(isHover);
+  };
   return (
-    <div style={{ border: '1px solid red' }}>
-      <div style={{ position: 'absolute', top: 32, left: 32 }}>
-        <SelectIcon />
-      </div>
-      <PhotoStyled src="https://cdn.pixabay.com/photo/2015/07/31/14/59/beach-869195_1280.jpg" />
-    </div>
+    <PhotoWrapperStyled
+      onMouseOver={() => onHover(true)}
+      onMouseLeave={() => onHover(false)}>
+      <SelectIconWrapperLayoutStyled>
+        <SelectIcon hover={hover} selected={selected} />
+      </SelectIconWrapperLayoutStyled>
+      <PhotoDisplayStyled src="https://cdn.pixabay.com/photo/2015/07/31/14/59/beach-869195_1280.jpg" />
+    </PhotoWrapperStyled>
   );
 };
 
--- src/modules/photos/components/Photo/styled.tsx
+++ src/modules/photos/components/Photo/styled.tsx
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
-export const PhotoStyled = styled.img`
+export const PhotoDisplayStyled = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+`;
+
+export const PhotoWrapperStyled = styled.div`
+  cursor: pointer;
+`;
+
+export const SelectIconWrapperLayoutStyled = styled.div`
+  position: absolute;
+  top: 36px;
+  left: 36px;
 `;
