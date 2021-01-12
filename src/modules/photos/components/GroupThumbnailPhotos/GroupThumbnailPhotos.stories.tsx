c81e6343466fa44903ef80bbaff331b50c94d788
yee2542 <yee2542gtl@gmail.com>
2021-01-13 04:31:51 +0700

    Add props

--- src/modules/photos/components/GroupThumbnailPhotos/GroupThumbnailPhotos.stories.tsx
+++ src/modules/photos/components/GroupThumbnailPhotos/GroupThumbnailPhotos.stories.tsx
@@ -3,13 +3,14 @@ import { Meta, Story } from '@storybook/react';
 import GroupThumbnailPhoto from '.';
 
 import { mock } from './constants';
+import { Props } from './types';
 
 export default {
   title: 'FluentSearch/GroupThumbnailPhoto',
   component: GroupThumbnailPhoto,
 } as Meta;
 
-const GroupThumbnailPhotoStory: Story = (args) => (
+const GroupThumbnailPhotoStory: Story<Props> = (args) => (
   <GroupThumbnailPhoto {...args} {...mock} />
 );
 
