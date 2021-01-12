907cd217ffa8b7fe87862c77a76d72d914a5e630
yee2542 <yee2542gtl@gmail.com>
2021-01-13 06:37:17 +0700

    Fix bad deps on test

--- src/modules/photos/components/GroupThumbnailPhotos/Header/Header.spec.tsx
+++ src/modules/photos/components/GroupThumbnailPhotos/Header/Header.spec.tsx
@@ -6,9 +6,14 @@ import { DateHeaderStyled } from './styled';
 
 describe('GroupThumbnailPhotos / Header Components', () => {
   const today = dayjs();
+  const mockOnSelect = () => {
+    return;
+  };
 
   it("Should show 'Today' when date is same date on props", () => {
-    const wrap = mount(<Header date={today.toDate()} />);
+    const wrap = mount(
+      <Header date={today.toDate()} selected={false} onSelect={mockOnSelect} />,
+    );
     const result = wrap.find(DateHeaderStyled).text();
     expect(result).toEqual('Today');
   });
@@ -16,7 +21,9 @@ describe('GroupThumbnailPhotos / Header Components', () => {
   it('Should show Locale date string when date is different date on props', () => {
     const date = today.add(1, 'day');
     const expected = date.format('ddd, MMM YYYY');
-    const wrap = mount(<Header date={date.toDate()} />);
+    const wrap = mount(
+      <Header date={date.toDate()} selected={false} onSelect={mockOnSelect} />,
+    );
     const result = wrap.find(DateHeaderStyled).text();
     expect(result).toEqual(expected);
   });
