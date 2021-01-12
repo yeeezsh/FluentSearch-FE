8a6aee6076a5d7e533c005b69cefded6196192da
yee2542 <yee2542gtl@gmail.com>
2021-01-13 03:26:56 +0700

    Implement mock src

--- src/modules/photos/components/ThumbnailPhoto/ThumbnailPhoto.stories.tsx
+++ src/modules/photos/components/ThumbnailPhoto/ThumbnailPhoto.stories.tsx
@@ -8,13 +8,19 @@ export default {
   component: ThumbnailPhoto,
 } as Meta;
 
+const baseArgs = {
+  src: 'https://cdn.pixabay.com/photo/2015/07/31/14/59/beach-869195_1280.jpg',
+} as Partial<Props>;
+
 const PhotoStory: Story<Props> = (args) => <ThumbnailPhoto {...args} />;
 export const Default = PhotoStory.bind({});
 Default.args = {
   selected: false,
+  ...baseArgs,
 } as Props;
 
 export const Selected = PhotoStory.bind({});
 Selected.args = {
   selected: true,
+  ...baseArgs,
 } as Props;
