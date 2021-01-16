5b9a56f57c2a6da9572a1659c2efcc995e0b6fc2
octobrekids <chnrd.k@gmail.com>
2021-01-17 02:56:42 +0700

    add layout

--- src/modules/dashboard/pages/index.tsx
+++ src/modules/dashboard/pages/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 
 export const DashboardPage: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Dashboard</h3>
+      <Row justify="center">
+        <Col>card1</Col>
+        <Col>card2</Col>
+        <Col>card3</Col>
+        <Col>card4</Col>
+      </Row>
+
+      <h3>Overview Album</h3>
+      <Row justify="center">
+        <Col>album1</Col>
+        <Col>album2</Col>
+        <Col>album3</Col>
+      </Row>
+    </div>
+  );
 };
