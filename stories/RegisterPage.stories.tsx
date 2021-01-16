ffa885d9aa26a64bd0f7b01e6f1e388f0bbfe2ac
octobrekids <33366852+octobrekids@users.noreply.github.com>
2021-01-17 02:32:59 +0700

    Fix-bad-style-storybook (#37)
    * fix bad margin and padding
    
    * remove unused space

--- stories/RegisterPage.stories.tsx
+++ stories/RegisterPage.stories.tsx
@@ -1,9 +1,15 @@
 import { Meta } from '@storybook/react';
 import RegisterPage from 'Modules/user/pages/register';
+import { GlobalStyle } from 'Styles/global';
 
 export default {
   title: 'FluentSearch/Pages/RegisterPage',
   component: RegisterPage,
 } as Meta;
 
-export const RegisterStories = (): JSX.Element => <RegisterPage />;
+export const RegisterStories = (): JSX.Element => (
+  <>
+    <GlobalStyle />
+    <RegisterPage />
+  </>
+);
