696237254dc1545ce64d5ed8ad3475e73fe3f069
octobrekids <chnrd.k@gmail.com>
2021-01-17 03:26:09 +0700

    add label global style

--- src/modules/dashboard/pages/index.tsx
+++ src/modules/dashboard/pages/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Progress } from 'antd';
 import { DashboardWrapper, ContentWrapper, DashboardCard } from './styled';
+import { Label } from 'Styles/global';
 
 const DashboardPage: React.FC = () => {
   return (
     <DashboardWrapper>
       <ContentWrapper>
         <h3 style={{ color: 'White' }}>Dashboard</h3>
-        <Row justify="center">
-          <Col md={3}>
+        <Row justify="space-around" align="middle">
+          <Col md={4}>
             <DashboardCard>
               <h5>Photos</h5>
               <br />
@@ -17,14 +18,31 @@ const DashboardPage: React.FC = () => {
               <p>+123.k</p>
             </DashboardCard>
           </Col>
-          <Col md={3}>
-            <DashboardCard>card1</DashboardCard>
+          <Col md={4}>
+            <DashboardCard>
+              <h5>Videos</h5>
+              <br />
+              <h3>123.k</h3>
+              <p>today upload</p>
+              <p>+123.k</p>
+            </DashboardCard>
           </Col>
-          <Col md={3}>
-            <DashboardCard>card1</DashboardCard>
+          <Col md={4}>
+            <DashboardCard>
+              <h5>Task</h5>
+              <br />
+              <Progress type="dashboard" percent={75} gapDegree={30} />
+              <p>100 of 1000 photos</p>
+            </DashboardCard>
           </Col>
-          <Col md={3}>
-            <DashboardCard>card1</DashboardCard>
+          <Col md={4}>
+            <DashboardCard>
+              <h5>Model</h5>
+              <br />
+              <Label color="purple">ResNet</Label>
+              <h3>800</h3>
+              <p>photos</p>
+            </DashboardCard>
           </Col>
         </Row>
 
