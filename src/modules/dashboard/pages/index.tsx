aa95117936666d4c14b77a5ff1d10eb6a5474010
octobrekids <chnrd.k@gmail.com>
2021-01-18 19:39:23 +0700

    add albumpreview and fix responsive card

--- src/modules/dashboard/pages/index.tsx
+++ src/modules/dashboard/pages/index.tsx
@@ -1,65 +1,112 @@
 import React from 'react';
 import { Row, Col, Progress } from 'antd';
-import { DashboardWrapper, ContentWrapper, DashboardCard, SmallBodyText } from './styled';
-import { Label } from 'Styles/global';
+import {
+  DashboardWrapper,
+  ContentWrapper,
+  DashboardCard,
+  SmallBodyText,
+  CardWrapper,
+  Label,
+} from './styled';
+import AlbumPreview from 'Modules/dashboard/components/AlbumPreview/index';
 
 const DashboardPage: React.FC = () => {
+  const LabelList: Array<string> = ['label1', 'label2', 'label3'];
+
   return (
     <DashboardWrapper>
       <ContentWrapper>
         <h3 style={{ color: 'White' }}>Dashboard</h3>
-        <Row justify="space-around" align="middle">
-          <Col md={4}>
-            <DashboardCard>
-              <h5>Photos</h5>
-              <br />
-              <h3>123.k</h3>
-              <SmallBodyText>today upload</SmallBodyText>
-              <p style={{ fontWeight: 'bold', color: '#5A36CC' }}>+123.k</p>
-            </DashboardCard>
-          </Col>
-          <Col md={4}>
-            <DashboardCard>
-              <h5>Videos</h5>
-              <br />
-              <h3>123.k</h3>
-              <SmallBodyText>today upload</SmallBodyText>
-              <p style={{ fontWeight: 'bold', color: '#5A36CC' }}>+123.k</p>
-            </DashboardCard>
-          </Col>
-          <Col md={4}>
-            <DashboardCard>
-              <h5>Task</h5>
-              <div style={{ textAlign: 'center' }}>
-                <Progress
-                  type="circle"
-                  percent={75}
-                  strokeWidth={8}
-                  strokeColor={{ '0%': '#5a36cc', '100%': '#5a36cc' }}
-                />
-                <SmallBodyText>100 of 1000 photos</SmallBodyText>
-              </div>
-            </DashboardCard>
-          </Col>
-          <Col md={4}>
-            <DashboardCard>
-              <h5>Model</h5>
-              <br />
-              <Label color="purple" style={{ marginTop: '10%' }}>
-                ResNet
-              </Label>
-              <h3 style={{ marginTop: '0%', marginBottom: '-3%' }}>800</h3>
-              <p>photos</p>
-            </DashboardCard>
-          </Col>
-        </Row>
+        <CardWrapper>
+          <Row justify="space-around" align="middle">
+            <Col md={4}>
+              <DashboardCard>
+                <h5>Photos</h5>
+                <br />
+                <h3>123.k</h3>
+                <SmallBodyText>today upload</SmallBodyText>
+                <p style={{ fontWeight: 'bold', color: '#5A36CC' }}>+123.k</p>
+              </DashboardCard>
+            </Col>
+            <Col md={4}>
+              <DashboardCard>
+                <h5>Videos</h5>
+                <br />
+                <h3>123.k</h3>
+                <SmallBodyText>today upload</SmallBodyText>
+                <p style={{ fontWeight: 'bold', color: '#5A36CC' }}>+123.k</p>
+              </DashboardCard>
+            </Col>
+            <Col md={4}>
+              <DashboardCard>
+                <h5>Task</h5>
+                <div style={{ textAlign: 'center' }}>
+                  <Progress
+                    type="circle"
+                    percent={75}
+                    strokeWidth={8}
+                    strokeColor={{ '0%': '#5a36cc', '100%': '#5a36cc' }}
+                  />
+                  <SmallBodyText style={{ marginTop: '10%' }}>
+                    100 of 1000 photos
+                  </SmallBodyText>
+                </div>
+              </DashboardCard>
+            </Col>
+            <Col md={4}>
+              <DashboardCard>
+                <h5>Model</h5>
+                <br />
+                <Label color="purple" style={{ marginTop: '10%' }}>
+                  ResNet
+                </Label>
+                <h3 style={{ marginTop: '0%', marginBottom: '-3%' }}>800</h3>
+                <p>photos</p>
+              </DashboardCard>
+            </Col>
+          </Row>
+        </CardWrapper>
 
-        <h3>Overview Album</h3>
-        <Row justify="center">
-          <Col>album1</Col>
-          <Col>album2</Col>
-          <Col>album3</Col>
-        </Row>
+        <div style={{ marginTop: '3%' }}>
+          <h3>Overview Album</h3>
+          <Row justify="space-around" align="middle">
+            <Col md={5}>
+              {' '}
+              <AlbumPreview
+                src="assets/images/test.jpg"
+                albumName="album name"
+                albumLength={1}
+                label={LabelList}
+              />
+            </Col>
+            <Col md={5}>
+              <AlbumPreview
+                src="assets/images/test.jpg"
+                albumName="album name"
+                albumLength={1}
+                label={LabelList}
+              />
+            </Col>
+            <Col md={5}>
+              {' '}
+              <AlbumPreview
+                src="assets/images/test.jpg"
+                albumName="album name"
+                albumLength={1}
+                label={LabelList}
+              />
+            </Col>
+            <Col md={5}>
+              {' '}
+              <AlbumPreview
+                src="assets/images/test.jpg"
+                albumName="album name"
+                albumLength={1}
+                label={LabelList}
+              />
+            </Col>
+          </Row>
+        </div>
       </ContentWrapper>
     </DashboardWrapper>
   );
--- src/modules/dashboard/pages/styled.tsx
+++ src/modules/dashboard/pages/styled.tsx
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
-import { Card } from 'antd';
+import { Card, Tag } from 'antd';
 
 export const DashboardWrapper = styled.div`
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 35%;
-  position: fixed;
-  background-color: #5a36cc;
-  z-index: -1;
+  margin-bottom: 50%;
+  height: 100vh;
 
   h1 {
     font-size: 3.05rem;
@@ -38,6 +33,7 @@ export const DashboardWrapper = styled.div`
   body,
   p {
     font-size: 1rem;
+    margin: 0;
   }
 `;
 
@@ -53,6 +49,12 @@ export const CaptionText = styled.p`
 `;
 
 export const ContentWrapper = styled.div`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 35%;
+  background-color: #5a36cc;
+  z-index: -1;
   padding: 8% 10% 10% 10%;
 `;
 
@@ -61,5 +63,14 @@ export const DashboardCard = styled(Card)`
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   height: auto;
   min-height: 250px;
-  padding-left: 2%;
+  width: auto;
+  min-width: 230px;
+`;
+
+export const CardWrapper = styled.div`
+  margin: 0% 3% 0% 0%;
+`;
+
+export const Label = styled(Tag)`
+  border-radius: 100px;
 `;
