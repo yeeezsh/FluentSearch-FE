f9f9f2a99843207852a2316ae270e24588372a25
octobrekids <chnrd.k@gmail.com>
2021-01-20 02:11:32 +0700

    fix global style constant

--- src/modules/home/components/Navbar/SideMenu/SideMenu.stories.tsx
+++ src/modules/home/components/Navbar/SideMenu/SideMenu.stories.tsx
@@ -4,13 +4,19 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 import React from 'react';
 import 'antd/dist/antd.css';
 import SideMenu, { SideMenuProps } from '.';
+import { GlobalStyle } from 'Styles/global';
 
 export default {
   title: 'FluentSearch/SideMenu',
   component: SideMenu,
 } as Meta;
 
-const SideMenuStory: Story<SideMenuProps> = (args): JSX.Element => <SideMenu {...args} />;
+const SideMenuStory: Story<SideMenuProps> = (args): JSX.Element => (
+  <>
+    <GlobalStyle />
+    <SideMenu {...args} />
+  </>
+);
 
 export const Default = SideMenuStory.bind({});
 Default.args = {
