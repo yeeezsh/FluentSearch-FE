1ccd6f6d2ea06a92fe72a83704b5f04c8720850a
octobrekids <chnrd.k@gmail.com>
2021-01-25 16:57:34 +0700

    add upload page layout

--- src/modules/upload/pages/index.tsx
+++ src/modules/upload/pages/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Col, Input, Layout, Row } from 'antd';
+import Button from 'Components/Button';
+import { UploadWrapper } from './styled';
 
 const UploadPage: React.FC = () => {
-  return <div></div>;
+  const { Content, Footer } = Layout;
+  return (
+    <Layout>
+      <UploadWrapper>
+        <Content>
+          <Row justify="space-between">
+            <Col md={8}>
+              <Input
+                size="large"
+                placeholder="Album name"
+                bordered={false}
+                style={{ borderBottom: '1px solid grey' }}
+              />
+            </Col>
+            <Col md={8}>sort</Col>
+          </Row>
+
+          <hr />
+          <p>upload photo</p>
+        </Content>
+      </UploadWrapper>
+      <Footer
+        style={{
+          backgroundColor: 'red',
+          boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px;',
+          position: 'fixed',
+          bottom: '0',
+          width: '100%',
+        }}>
+        <Button>Next &gt;</Button>
+      </Footer>
+    </Layout>
+  );
 };
 
 export default UploadPage;
---
+++ src/modules/upload/pages/styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const UploadWrapper = styled.div`
+  height: 100vh;
+  padding: 5%;
+  background-color: white;
+
+  .ant-layout-content {
+    background-color: white;
+  }
+`;
