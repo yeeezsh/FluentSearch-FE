ee1b49b73cba3cb6fcc1e159388b9a8591d2ed62
yee2542 <yee2542gtl@gmail.com>
2021-01-28 02:18:54 +0700

    REFACOTR

--- src/modules/photos/reducers/photosReducer/actions.ts
+++ src/modules/photos/reducers/photosReducer/actions.ts
@@ -1,4 +1,4 @@
-import { AsyncThunk, createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { photosSlice } from '.';
 import { PHOTOS } from './types';
