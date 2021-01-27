b401d3aa35739ceb1670d9ef8c304cafb6888798
yee2542 <yee2542gtl@gmail.com>
2021-01-28 01:19:23 +0700

    update PhotoState

--- src/modules/photos/reducers/photosReducer/types.ts
+++ src/modules/photos/reducers/photosReducer/types.ts
@@ -1,3 +1,18 @@
+import { Album } from 'Modules/photos/models/album';
+import { Photo } from 'Modules/photos/models/photo';
+import { GroupThumbnailPhotos, ThumbnailPhoto } from 'Modules/photos/models/thumbnail';
+
 export type PhotoState = {
-  // data:
+  data: {
+    photos: Photo[];
+    albums: Album[];
+  };
+  presentation: {
+    thumbnail: ThumbnailPhoto[];
+    thumbnailGroup: GroupThumbnailPhotos[];
+    views?: {
+      photo: Photo;
+      album: Album;
+    };
+  };
 };
