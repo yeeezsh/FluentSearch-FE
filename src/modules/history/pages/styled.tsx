c9e85dc673f72402d9d9f7cb07a5c5a88fa531fc
octobrekids <chnrd.k@gmail.com>
2021-01-28 02:00:39 +0700

    set style pagination

--- src/modules/history/pages/styled.tsx
+++ src/modules/history/pages/styled.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
 export const PageWrapper = styled.div`
-  margin: 10% 10% 10% 10%;
+  margin: 10% 10% 0% 10%;
+  padding-bottom: 5%;
   .ant-table-thead > tr > th {
     background: #e1d8ff;
+    color: #5a36cc;
+  }
+  .ant-pagination-item-active {
+    border: 1px solid #5a36cc;
+  }
+
+  .ant-pagination-item-active a {
     color: #5a36cc;
   }
 `;
