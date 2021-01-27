a95adb5321cb66151d96cd330409a84fd3a0cb21
yee2542 <yee2542gtl@gmail.com>
2021-01-28 04:06:02 +0700

    refactor

--- src/modules/photos/reducers/photosReducer/actions.ts
+++ src/modules/photos/reducers/photosReducer/actions.ts
@@ -3,13 +3,8 @@ import { Photos } from 'Modules/photos/constants/photo/interface';
 import { fetchImages } from 'Modules/photos/services/fetch.images';
 import { PHOTOS } from './types';
 
-const mapActionName = (action: string) => `${PHOTOS}/${action}`;
-
-export const fetchPhotosData = createAsyncThunk(
-  mapActionName('fetchPhotosData'),
-  async (id?: string) => {
-    return { data: (await fetchImages()).map((el) => ({ ...el, _id: el.id })) } as {
-      data: Array<Photos & { _id: string }>;
-    };
-  },
-);
+export const fetchPhotosData = createAsyncThunk(PHOTOS, async (id?: string) => {
+  return { data: (await fetchImages()).map((el) => ({ ...el, _id: el.id })) } as {
+    data: Array<Photos & { _id: string }>;
+  };
+});
