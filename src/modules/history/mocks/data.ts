bbe994c904daf566fcf05b4b064775b888917c01
octobrekids <chnrd.k@gmail.com>
2021-01-28 01:36:59 +0700

    add mock history data

--- src/modules/history/mocks/data.ts
+++ src/modules/history/mocks/data.ts
@@ -1,14 +1,30 @@
-export const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
+import * as Factory from 'factory.ts';
+import faker from 'faker';
+import { StatusEnum } from 'Modules/history/models/status.enum';
+
+enum ModelEnum {
+  RESNET = 'ResNet',
+  VGG = 'VGG16',
+}
+
+interface IDataSource {
+  key: string;
+  taskID: string;
+  taskName: string;
+  model: ModelEnum;
+  startTime: string;
+  finishTime: string;
+  status: StatusEnum;
+}
+
+const DataSourceMock = Factory.Sync.makeFactory<IDataSource>({
+  key: Factory.each(() => faker.random.uuid()),
+  taskID: Factory.each(() => faker.random.uuid()),
+  taskName: Factory.each(() => faker.lorem.word()),
+  model: Factory.each(() => faker.random.arrayElement(Object.values(ModelEnum))),
+  startTime: Factory.each(() => faker.date.past().toString()),
+  finishTime: Factory.each(() => faker.date.past().toString()),
+  status: Factory.each(() => faker.random.arrayElement(Object.values(StatusEnum))),
+});
+
+export const DataSource: IDataSource[] = DataSourceMock.buildList(10);
