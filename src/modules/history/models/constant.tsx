1ecdd808b3355635c0f4130a0aec855635418f91
octobrekids <chnrd.k@gmail.com>
2021-01-28 01:37:34 +0700

    add render taskID and status

--- src/modules/history/models/constant.tsx
+++ src/modules/history/models/constant.tsx
@@ -1,8 +1,15 @@
+import { Tag } from 'antd';
+import { stringCutter } from '../util/stringCutter';
+import { StatusEnum } from './status.enum';
+
 export const columns = [
   {
     title: 'TaskID',
     dataIndex: 'taskID',
     key: 'taskID',
+    render: function showTaskID(taskID: string): string {
+      return stringCutter(taskID);
+    },
   },
   {
     title: 'Task Name',
@@ -28,5 +35,12 @@ export const columns = [
     title: 'Status',
     dataIndex: 'status',
     key: 'status',
+    render: function showStatus(status: string): JSX.Element {
+      return (
+        <Tag color={'geekblue'} key={status}>
+          {status.toUpperCase()}
+        </Tag>
+      );
+    },
   },
 ];
