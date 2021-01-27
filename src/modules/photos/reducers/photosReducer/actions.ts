48722af84168150cbbb153539306f57fc0b4114c
yee2542 <yee2542gtl@gmail.com>
2021-01-28 02:16:00 +0700

    add simple thunk

--- src/modules/photos/reducers/photosReducer/actions.ts
+++ src/modules/photos/reducers/photosReducer/actions.ts
@@ -1,3 +1,24 @@
+import { AsyncThunk, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { photosSlice } from '.';
+import { PHOTOS } from './types';
 
 export default photosSlice.actions;
+
+const mapActionName = (action: string) => `${PHOTOS}/${action}`;
+
+export const fetchPhotosData = createAsyncThunk(
+  mapActionName('fetchPhotosData'),
+  async (id?: string) => {
+    const fetchImages = async (): Promise<any> => {
+      const apiRoot = 'https://api.unsplash.com';
+      const accessKey = 'fLLHNmXzlY1Mkc9woN0pQFNNc53hoBfGAgmQTF2OH4w';
+      const { data } = await axios.get(
+        `${apiRoot}/photos/random?client_id=${accessKey}&count=5`,
+      );
+      return data;
+    };
+
+    return { data: await fetchImages() } as { data: any };
+  },
+);
--- src/modules/photos/reducers/photosReducer/index.ts
+++ src/modules/photos/reducers/photosReducer/index.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchPhotosData } from './actions';
 import { initPhotosState } from './init';
 import { PHOTOS } from './types';
 
@@ -9,6 +10,18 @@ export const photosSlice = createSlice({
     init(state) {
       return { ...state, ...initPhotosState };
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchPhotosData.pending, (state) => {
+      state.data.ready = false;
+      state.error = undefined;
+    });
+    builder.addCase(fetchPhotosData.fulfilled, (state, action) => {
+      state.data.ready = true;
+      state.error = undefined;
+      state.data.photos = action.payload.data;
+      console.log(action.payload.data);
+    });
   },
 });
 
