e4e51a20df39760ceb8a5b2e71cf6c994230e154
octobrekids <chnrd.k@gmail.com>
2021-01-28 02:08:35 +0700

    move PageWrapper and Table to global style

--- src/common/styles/global.tsx
+++ src/common/styles/global.tsx
@@ -1,5 +1,6 @@
 import 'antd/dist/antd.css';
 import styled, { createGlobalStyle, css } from 'styled-components';
+import { Table } from 'antd';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
@@ -130,4 +131,23 @@ const handleAlignType = (align?: PComponentProps['align']) => {
 export const P = styled.p<PComponentProps>`
   ${({ align }) => handleAlignType(align)};
   color: darkgray;
+`;
+
+export const PurpleTable = styled(Table)`
+  .ant-table-thead > tr > th {
+    background: #e1d8ff;
+    color: #5a36cc;
+  }
+  .ant-pagination-item-active {
+    border: 1px solid #5a36cc;
+  }
+
+  .ant-pagination-item-active a {
+    color: #5a36cc;
+  }
+`;
+
+export const PageWrapper = styled.div`
+  margin: 10% 10% 0% 10%;
+  padding-bottom: 5%;
 `;
--- src/modules/history/pages/index.tsx
+++ src/modules/history/pages/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { DataSource } from 'Modules/history/mocks/data';
 import { columns } from 'Modules/history/models/constant';
-import { Table } from 'antd';
-import { PageWrapper } from './styled';
+import { PurpleTable, PageWrapper } from 'Styles/global';
 const HistoryPages: React.FC = () => {
   return (
     <PageWrapper>
       <h3>History</h3>
       <hr />
-      <Table
+      <PurpleTable
         dataSource={DataSource}
         columns={columns}
         style={{ marginTop: '30px' }}
