7a78fd2121ef04cce3182876ba053689ab921638
yee2542 <yee2542gtl@gmail.com>
2021-01-28 03:38:17 +0700

    Add simple selected photos

--- src/modules/photos/reducers/photosReducer/index.ts
+++ src/modules/photos/reducers/photosReducer/index.ts
@@ -1,5 +1,8 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Album } from 'Modules/photos/models/album';
+import { Photo } from 'Modules/photos/models/photo';
 import { ErrorStateCodeEnum } from 'Stores/common/types/error';
+import withPayload from 'Stores/common/withPayload';
 import { fetchPhotosData } from './actions';
 import { initPhotosState } from './init';
 import { PHOTOS } from './types';
@@ -10,6 +13,12 @@ export const photosSlice = createSlice({
   reducers: {
     init(state) {
       return { ...state, ...initPhotosState };
+    },
+    selectPhotos(state, action: PayloadAction<{ photo: Photo; album: Album }>) {
+      state.presentation.views = {
+        photo: action.payload.photo,
+        album: action.payload.album,
+      };
     },
   },
   extraReducers: (builder) => {
