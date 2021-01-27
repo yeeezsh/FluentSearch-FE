34c109fa40c732729241f5f22fa157a8ebb3d6de
octobrekids <chnrd.k@gmail.com>
2021-01-28 03:42:54 +0700

    add ant table cell align center

--- src/common/styles/global.tsx
+++ src/common/styles/global.tsx
@@ -136,13 +136,17 @@ export const P = styled.p<PComponentProps>`
 export const PurpleTable = styled(Table)`
   .ant-table-thead > tr > th {
     background: #e1d8ff;
-    color: #5a36cc;
+    color: var(--secondary-color);
   }
   .ant-pagination-item-active {
-    border: 1px solid #5a36cc;
+    border: 1px solid var(--secondary-color);
   }
   .ant-pagination-item-active a {
-    color: #5a36cc;
+    color: var(--secondary-color);
+  }
+
+  .ant-table-cell {
+    text-align: center;
   }
 `;
 
