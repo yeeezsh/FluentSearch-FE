68472859a3b94fde24319864c848f6e457cc9c31
octobrekids <chnrd.k@gmail.com>
2021-01-28 01:37:43 +0700

    import mock data

--- src/modules/history/pages/index.tsx
+++ src/modules/history/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { dataSource } from 'Modules/history/mocks/data';
+import { DataSource } from 'Modules/history/mocks/data';
 import { columns } from 'Modules/history/models/constant';
 import { Table } from 'antd';
 import { PageWrapper } from './styled';
 const HistoryPages: React.FC = () => {
   return (
     <PageWrapper>
-      <Table dataSource={dataSource} columns={columns} />
+      <Table dataSource={DataSource} columns={columns} />
     </PageWrapper>
   );
 };
