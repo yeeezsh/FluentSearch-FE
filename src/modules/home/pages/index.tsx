6bb5501ffeacfc09183f06ba6070aaf5b413bbbb
octobrekids <chnrd.k@gmail.com>
2021-01-28 21:37:17 +0700

    restore home page

--- src/modules/home/pages/index.tsx
+++ src/modules/home/pages/index.tsx
@@ -1,11 +1,56 @@
+import { Card, Col, Row } from 'antd';
+import Button from 'Components/Button';
+import HomeLayout from 'Modules/home/components/Layouts';
+import { APP_FEATURE_CONSTANT } from 'Modules/home/constants/feature';
 import React from 'react';
-import { HomeTempWrapper } from './styled';
+import IconSelector from 'Utils/icon-selector';
+import {
+  CanvasWrapper,
+  gridStyle,
+  HeaderWrapper,
+  Heading,
+  Icon,
+  SubHeading,
+} from './styled';
+
+const FeatureCard: React.FC = () => {
+  return (
+    <>
+      {APP_FEATURE_CONSTANT.map(({ key, icon, label, description }) => (
+        <Col xs={20} md={5} lg={7} key={key}>
+          <Card style={gridStyle}>
+            <Icon>
+              <IconSelector type={icon} />
+            </Icon>
+            <br />
+            <h2>{label}</h2>
+            <p>{description}</p>
+          </Card>
+        </Col>
+      ))}
+    </>
+  );
+};
 
 const HomePage: React.FC = () => {
   return (
-    <>
-      <HomeTempWrapper></HomeTempWrapper>
-    </>
+    <HomeLayout>
+      <CanvasWrapper>
+        <HeaderWrapper>
+          <Heading>Heading</Heading>
+          <SubHeading>Lorem ipsum..</SubHeading>
+          <Button>Explore</Button>
+        </HeaderWrapper>
+      </CanvasWrapper>
+
+      <Row gutter={[0, 24]} justify="space-around" style={{ marginTop: '20px' }}>
+        <Col span={21}>
+          <Row justify="center" gutter={[24, 24]}>
+            <FeatureCard />
+          </Row>
+        </Col>
+      </Row>
+    </HomeLayout>
   );
 };
 
