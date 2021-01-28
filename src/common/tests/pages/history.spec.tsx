96e568acd529fd05d35a5654a3097156bb3000b6
octobrekids <chnrd.k@gmail.com>
2021-01-28 13:44:04 +0700

    add columns test

--- src/common/tests/pages/history.spec.tsx
+++ src/common/tests/pages/history.spec.tsx
@@ -1,4 +1,4 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 import HistoryPage from 'Modules/history/pages';
 import { PurpleTable } from 'Styles/global';
@@ -12,5 +12,15 @@ describe('Pages/History test', () => {
   it('Should contain PurpleTable', () => {
     const wrap = shallow(<HistoryPage />);
     expect(wrap.exists(PurpleTable)).toBe(true);
+  });
+
+  it('Should contain Timestamp, TaskID, TaskName, Model, Start Time, Finish Time and Status Columns', () => {
+    const wrap = mount(<HistoryPage />);
+    expect(wrap.find('th').at(0).text()).toEqual('TaskID');
+    expect(wrap.find('th').at(1).text()).toEqual('Task Name');
+    expect(wrap.find('th').at(2).text()).toEqual('Model');
+    expect(wrap.find('th').at(3).text()).toEqual('Start Time');
+    expect(wrap.find('th').at(4).text()).toEqual('Finish Time');
+    expect(wrap.find('th').at(5).text()).toEqual('Status');
   });
 });
