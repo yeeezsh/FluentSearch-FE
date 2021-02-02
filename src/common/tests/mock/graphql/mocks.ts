e0aa3823e50675c5564185f248da32e44d18b219
yee2542 <yee2542gtl@gmail.com>
2021-02-02 23:27:58 +0700

    fix bad import

--- src/common/tests/mock/graphql/mocks.ts
+++ src/common/tests/mock/graphql/mocks.ts
@@ -1,4 +1,5 @@
-import * as Factory from 'factory.ts';
+import { Sync, each } from 'factory.ts';
+
 import faker from 'faker';
 import {
   UserWithId,
@@ -16,54 +17,54 @@ import {
 import { MockedResponse } from '@apollo/client/testing';
 import { GET_USER } from './query';
 
-export const AppModelMock = Factory.Sync.makeFactory<AppModel>({
+export const AppModelMock = Sync.makeFactory<AppModel>({
   __typename: 'AppModel',
-  status: Factory.each(() => faker.random.number()),
+  status: each(() => faker.random.number()),
 });
 
-export const QueryUserArgsMock = Factory.Sync.makeFactory<QueryUserArgs>({
-  id: Factory.each(() => faker.random.uuid()),
+export const QueryUserArgsMock = Sync.makeFactory<QueryUserArgs>({
+  id: each(() => faker.random.uuid()),
 });
 
-export const QueryUsersArgsMock = Factory.Sync.makeFactory<QueryUsersArgs>({
-  limit: Factory.each(() => faker.random.number()),
-  skip: Factory.each(() => faker.random.number()),
+export const QueryUsersArgsMock = Sync.makeFactory<QueryUsersArgs>({
+  limit: each(() => faker.random.number()),
+  skip: each(() => faker.random.number()),
 });
 
-export const UserRegisterInputMock = Factory.Sync.makeFactory<UserRegisterInput>({
-  mainEmail: Factory.each(() => faker.internet.email()),
-  name: Factory.each(() => faker.name.firstName()),
-  password: Factory.each(() => faker.internet.password()),
+export const UserRegisterInputMock = Sync.makeFactory<UserRegisterInput>({
+  mainEmail: each(() => faker.internet.email()),
+  name: each(() => faker.name.firstName()),
+  password: each(() => faker.internet.password()),
 });
 
-export const UserTokenMock = Factory.Sync.makeFactory<UserToken>({
+export const UserTokenMock = Sync.makeFactory<UserToken>({
   __typename: 'UserToken',
-  provider: Factory.each(() => faker.lorem.word()),
-  token: Factory.each(() => faker.lorem.word()),
+  provider: each(() => faker.lorem.word()),
+  token: each(() => faker.lorem.word()),
 });
 
-export const UserUpdateInputMock = Factory.Sync.makeFactory<UserUpdateInput>({
-  id: Factory.each(() => faker.random.uuid()),
-  mainEmail: Factory.each(() => faker.internet.email()),
-  name: Factory.each(() => faker.name.firstName()),
+export const UserUpdateInputMock = Sync.makeFactory<UserUpdateInput>({
+  id: each(() => faker.random.uuid()),
+  mainEmail: each(() => faker.internet.email()),
+  name: each(() => faker.name.firstName()),
 });
 
-export const UserWithIdMock = Factory.Sync.makeFactory<UserWithId>({
+export const UserWithIdMock = Sync.makeFactory<UserWithId>({
   __typename: 'UserWithId',
-  _id: Factory.each(() => faker.random.uuid()),
-  name: Factory.each(() => faker.name.firstName()),
-  mainEmail: Factory.each(() => faker.internet.email()),
-  deactivate: Factory.each(() => faker.random.boolean()),
-  email: Factory.each(() => {
+  _id: each(() => faker.random.uuid()),
+  name: each(() => faker.name.firstName()),
+  mainEmail: each(() => faker.internet.email()),
+  deactivate: each(() => faker.random.boolean()),
+  email: each(() => {
     return new Array(2).fill(faker.internet.email());
   }),
-  createDate: Factory.each(() => faker.date.past().toString()),
-  oauth: Factory.each(() => UserTokenMock.build()),
-  package: Factory.each(() => faker.random.arrayElement(Object.values(UserPackageEnum))),
-  password: Factory.each(() => faker.internet.password()),
-  role: Factory.each(() => faker.random.arrayElement(Object.values(UserRoleEnum))),
-  updateDate: Factory.each(() => faker.date.past().toString()),
-  zone: Factory.each(() => faker.random.arrayElement(Object.values(UserZoneEnum))),
+  createDate: each(() => faker.date.past().toString()),
+  oauth: each(() => UserTokenMock.build()),
+  package: each(() => faker.random.arrayElement(Object.values(UserPackageEnum))),
+  password: each(() => faker.internet.password()),
+  role: each(() => faker.random.arrayElement(Object.values(UserRoleEnum))),
+  updateDate: each(() => faker.date.past().toString()),
+  zone: each(() => faker.random.arrayElement(Object.values(UserZoneEnum))),
 });
 
 export const usersQueryMock: MockedResponse<Query> = {
