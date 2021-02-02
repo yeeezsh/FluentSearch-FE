eefe2f1175086a62bded192280378ed84469807b
octobrekids <chnrd.k@gmail.com>
2021-02-03 05:31:49 +0700

    add reducer to task page

--- src/modules/task/pages/index.tsx
+++ src/modules/task/pages/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { DataSource } from 'Modules/task/mocks/data';
+import React, { useEffect } from 'react';
 import { columns } from 'Modules/task/models/constant';
 import { PurpleTable, PageWrapper } from 'Styles/global';
+import { fetchTaskData } from '../reducer/taskReducer/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { StoresState } from 'Stores/index';
 const TaskPage: React.FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTaskData());
+  }, []);
+  const taskData = useSelector((state: StoresState) => state.task);
   return (
     <PageWrapper>
       <h3>Task</h3>
       <hr />
       <PurpleTable
-        dataSource={DataSource}
+        dataSource={taskData.data}
         columns={columns}
         style={{ marginTop: '30px' }}
         pagination={{ position: ['bottomCenter'] }}
