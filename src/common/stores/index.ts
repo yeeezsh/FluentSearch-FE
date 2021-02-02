64ea088afcb77f835b06cebec47f15234acd0db6
octobrekids <chnrd.k@gmail.com>
2021-02-03 05:24:52 +0700

    add taskReducer to rootReducer

--- src/common/stores/index.ts
+++ src/common/stores/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore, Middleware } from '@reduxjs/toolkit';
+import taskReducer from 'Modules/task/reducer/taskReducer';
 import userReducer from 'Modules/user/reducers/userReducer';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  task: taskReducer,
 });
 
 export const middleware: ThunkMiddleware[] | Middleware[] = [thunk];
