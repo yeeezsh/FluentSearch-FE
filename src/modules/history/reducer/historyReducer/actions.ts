1589fb747677c705f31703ea560f98442f402740
octobrekids <chnrd.k@gmail.com>
2021-02-03 04:00:47 +0700

    remove unnecessary code

--- src/modules/history/reducer/historyReducer/actions.ts
+++ src/modules/history/reducer/historyReducer/actions.ts
@@ -2,6 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { fetchHistory } from 'Modules/history/services/fetch.history';
 import { HISTORY } from './types';
 
-export const fetchHistoryData = createAsyncThunk(HISTORY, async (key?: string) => {
+export const fetchHistoryData = createAsyncThunk(HISTORY, async () => {
   return { data: (await fetchHistory()).map((el) => ({ ...el, key: el.key })) };
 });
