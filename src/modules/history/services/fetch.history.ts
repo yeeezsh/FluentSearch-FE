c95c9e01c3e5a7c858778bdb3dd162baffb17bce
octobrekids <chnrd.k@gmail.com>
2021-02-03 03:58:32 +0700

    fix to mock fetch data

--- src/modules/history/services/fetch.history.ts
+++ src/modules/history/services/fetch.history.ts
@@ -1,8 +1,13 @@
-import axios from 'axios';
-import { HistoryData } from '../pages/types';
+// import axios from 'axios';
+import { DataSource, IDataSource } from '../mocks/data';
+// import { HistoryData } from '../pages/types';
 
-export const fetchHistory = async (): Promise<HistoryData[]> => {
-  const apiRoot = 'https://6019ba397db53900178346f2.mockapi.io/History';
-  const { data } = await axios.get(apiRoot);
-  return data;
+// export const fetchHistory = async (): Promise<HistoryData[]> => {
+//   const apiRoot = 'https://6019ba397db53900178346f2.mockapi.io/History';
+//   const { data } = await axios.get(apiRoot);
+//   return data;
+// };
+
+export const fetchHistory = (): IDataSource[] => {
+  return DataSource;
 };
