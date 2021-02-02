827b773e159f18e5d40d9cde441832cf5652a675
octobrekids <chnrd.k@gmail.com>
2021-02-03 03:59:05 +0700

    change history init state

--- src/modules/history/reducer/historyReducer/init.ts
+++ src/modules/history/reducer/historyReducer/init.ts
@@ -1,16 +1,6 @@
-import { ModelEnum } from 'Modules/history/models/model.enum';
-import { StatusEnum } from 'Modules/history/models/status.enum';
 import { HistoryState } from './types';
 
 export const initHistoryState: HistoryState = {
-  data: {
-    key: '',
-    taskID: '',
-    taskName: '',
-    model: ModelEnum.RESNET,
-    startTime: '',
-    finishTime: '',
-    status: StatusEnum.FINISH,
-    ready: false,
-  },
+  data: [],
+  ready: false,
 };
