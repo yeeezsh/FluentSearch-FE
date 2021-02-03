050d58d3f5f1999e29a4661b7c584eb79bbd3181
octobrekids <chnrd.k@gmail.com>
2021-02-03 19:52:51 +0700

    add dashboardReducer

---
+++ src/modules/dashboard/reducer/dashboardReducer/index.ts
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { initDashboardState } from './init';
+import { DASHBOARD } from '../../models/types';
+import { fetchDashboardData } from './actions';
+import { ErrorStateCodeEnum } from 'Stores/common/types/error';
+
+export const dashboardSlice = createSlice({
+  name: DASHBOARD,
+  initialState: initDashboardState,
+  reducers: {
+    init(state) {
+      return { ...state, ...initDashboardState };
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchDashboardData.rejected, (state) => {
+      state.ready = false;
+      state = {
+        ...state,
+        error: {
+          code: ErrorStateCodeEnum.ServerInternalError,
+          msg: 'api error',
+        },
+      };
+    });
+    builder.addCase(fetchDashboardData.pending, (state) => {
+      state.ready = false;
+      state.error = undefined;
+    });
+    builder.addCase(fetchDashboardData.fulfilled, (state, action) => {
+      state.ready = true;
+      state.error = undefined;
+      state.presentation = action.payload.data;
+    });
+  },
+});
