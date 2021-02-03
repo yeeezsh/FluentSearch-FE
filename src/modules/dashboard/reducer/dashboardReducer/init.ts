63eb69acd1c1ac3422406b0be90243ce5062d089
octobrekids <chnrd.k@gmail.com>
2021-02-03 19:30:38 +0700

    change init state

--- src/modules/dashboard/reducer/dashboardReducer/init.ts
+++ src/modules/dashboard/reducer/dashboardReducer/init.ts
@@ -1,20 +1,18 @@
 import { ModelEnum } from 'Modules/dashboard/constants/model.enum';
-import { DashboardState } from './types';
+import { DashboardState } from 'Modules/dashboard/models/types';
 
 export const initDashboardState: DashboardState = {
-  data: [
-    {
-      totalPhotos: 0,
-      totalVideos: 0,
-      todayPhotos: 0,
-      todayVideos: 0,
-      upcomingModel: '-',
-      model: ModelEnum.NONE,
-      processWithModelPhoto: 0,
-      finishRunningPhotos: 0,
-      totalRunningPhotos: 0,
-      progressPhoto: 0,
-    },
-  ],
+  presentation: {
+    totalPhotos: 0,
+    totalVideos: 0,
+    todayPhotos: 0,
+    todayVideos: 0,
+    upcomingModel: '-',
+    model: ModelEnum.NONE,
+    processWithModelPhoto: 0,
+    finishRunningPhotos: 0,
+    totalRunningPhotos: 0,
+    progressPhoto: 0,
+  },
   ready: false,
 };
