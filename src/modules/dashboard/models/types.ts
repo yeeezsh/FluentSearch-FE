e780de5a0b09ef7d3ca2d8d798951e512b975eea
octobrekids <chnrd.k@gmail.com>
2021-02-03 18:45:24 +0700

    move type to models

--- src/modules/dashboard/models/types.ts
+++ src/modules/dashboard/models/types.ts
@@ -5,20 +5,18 @@ import { ErrorState } from 'Stores/common/error';
 export const DASHBOARD = 'DASHBOARD';
 
 export type DashboardState = {
-  data: [
-    {
-      totalPhotos: number;
-      totalVideos: number;
-      todayPhotos: number;
-      todayVideos: number;
-      upcomingModel: string;
-      model: ModelEnum;
-      processWithModelPhoto: number;
-      finishRunningPhotos: number;
-      totalRunningPhotos: number;
-      progressPhoto: number;
-    },
-  ];
+  presentation: {
+    totalPhotos: number;
+    totalVideos: number;
+    todayPhotos: number;
+    todayVideos: number;
+    upcomingModel: string;
+    model: ModelEnum;
+    processWithModelPhoto: number;
+    finishRunningPhotos: number;
+    totalRunningPhotos: number;
+    progressPhoto: number;
+  };
   ready: boolean;
   error?: ErrorState;
 };
