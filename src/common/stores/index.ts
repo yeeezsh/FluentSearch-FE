4431dfbb346b2cc5be8689cd94919f891b668cfa
octobrekids <chnrd.k@gmail.com>
2021-02-03 21:19:00 +0700

    add reducer to root reducer

--- src/common/stores/index.ts
+++ src/common/stores/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore, Middleware } from '@reduxjs/toolkit';
+import dashboardReducer from 'Modules/dashboard/reducer/dashboardReducer';
 import userReducer from 'Modules/user/reducers/userReducer';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  dashboard: dashboardReducer,
 });
 
 export const middleware: ThunkMiddleware[] | Middleware[] = [thunk];
