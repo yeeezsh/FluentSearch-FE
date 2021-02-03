7819f3e0119364b9c014e68ae81bf69de9efe8a7
octobrekids <chnrd.k@gmail.com>
2021-02-03 19:59:41 +0700

    add todo

--- src/modules/dashboard/components/AlbumPreview/types.ts
+++ src/modules/dashboard/components/AlbumPreview/types.ts
@@ -4,6 +4,6 @@ export type AlbumPreviewProps = {
   albumName: string;
   albumLength: number;
   label: Array<string>;
-  // TODO - required link when have a real props link
+  //TODO: - required link when have a real props link
   link?: string;
 };
