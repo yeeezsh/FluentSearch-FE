ff8a3e7c03d42e4641a3a4fc792fd741853202f8
octobrekids <chnrd.k@gmail.com>
2021-02-03 19:52:40 +0700

    add actions

---
+++ src/modules/dashboard/reducer/dashboardReducer/actions.ts
@@ -0,0 +1,7 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchDashboard } from 'Modules/dashboard/services/fetch.dashboardData';
+import { DASHBOARD } from '../../models/types';
+
+export const fetchDashboardData = createAsyncThunk(DASHBOARD, async () => {
+  return { data: await fetchDashboard() };
+});
