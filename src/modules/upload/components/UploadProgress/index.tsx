1a46d6729247df7ec7187e4629f25763277aa72b
octobrekids <chnrd.k@gmail.com>
2021-02-05 03:49:37 +0700

    add  upload progress

---
+++ src/modules/upload/components/UploadProgress/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { size } from 'lodash';
+import UploadItem from './UploadItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { uploadPhotoData } from 'Modules/upload/reducer/uploadReducer/actions';
+import { StoresState } from 'Stores/index';
+import { Wrapper } from './styled';
+
+const UploadProgress: React.FC = () => {
+  const dispatch = useDispatch();
+  const fileProgress = useSelector(
+    (state: StoresState) => state.upload.data.fileProgress,
+  );
+  const uploadedFileAmount = size(fileProgress);
+
+  useEffect(() => {
+    const fileToUpload = Object.values(fileProgress).filter(
+      (file) => file.progress === 0,
+    );
+    dispatch(uploadPhotoData(fileToUpload));
+  }, [uploadedFileAmount]);
+
+  return uploadedFileAmount > 0 ? (
+    <Wrapper>
+      <h4>Uploading File</h4>
+      {size(fileProgress)
+        ? Object.values(fileProgress).map((file) => {
+            <UploadItem
+              key={file._id}
+              file={{ file: file.file, progress: file.progress }}
+            />;
+          })
+        : null}
+    </Wrapper>
+  ) : null;
+};
+
+export default UploadProgress;
