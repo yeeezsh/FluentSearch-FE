0aa0b1480ac25d04ce79b6150c169ef838e78757
octobrekids <chnrd.k@gmail.com>
2021-02-05 01:34:59 +0700

    refactor

--- src/modules/upload/reducer/uploadReducer/actions.ts
+++ src/modules/upload/reducer/uploadReducer/actions.ts
@@ -2,6 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { UPLOAD } from 'Modules/upload/model/types';
 import { uploadPhoto } from 'Modules/upload/services/upload.photo';
 
-export const uploadPhotoData = createAsyncThunk(UPLOAD, async (formPayload: any) => {
-  return { data: await uploadPhoto(formPayload) };
+export const uploadPhotoData = createAsyncThunk(UPLOAD, async (fileToUpload) => {
+  return { data: await uploadPhoto(fileToUpload) };
 });
--- src/modules/upload/services/upload.photo.ts
+++ src/modules/upload/services/upload.photo.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 //TODO: Change to correct type
-export const uploadPhoto = async (owner: string, formPayload: any): Promise<any> => {
+export const uploadPhoto = async (fileToUpload: any): Promise<any> => {
   const files = new FormData();
-  files.append('file', formPayload);
+  files.append('file', fileToUpload.data);
   const url = 'http://localhost:5000/file';
   const config = {
     headers: {
@@ -12,7 +12,7 @@ export const uploadPhoto = async (owner: string, formPayload: any): Promise<any>
     },
   };
   const data = {
-    owner: owner,
+    owner: fileToUpload.owner,
     files: files,
   };
   const response = await axios.post(url, data, config);
