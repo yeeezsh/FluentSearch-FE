9acd8d8a4c64b272d1891c60cb37fcde89df9378
octobrekids <chnrd.k@gmail.com>
2021-02-04 23:59:29 +0700

    add uploadFile util

---
+++ src/modules/upload/utils/uploadFile.utils.ts
@@ -0,0 +1,25 @@
+import { size } from 'lodash';
+import { fileProgressType } from '../model/types';
+
+export const modifyFiles = (
+  existingFiles: fileProgressType[],
+  files: File[],
+): fileProgressType[] => {
+  let fileToUpload: fileProgressType[] = [];
+
+  for (let i = 0; i < files.length; i++) {
+    const _id = size(existingFiles) + i + 1;
+
+    fileToUpload = [
+      ...fileToUpload,
+      {
+        _id: _id,
+        file: files[i],
+        progress: 0,
+        status: 0,
+      },
+    ];
+  }
+
+  return fileToUpload;
+};
