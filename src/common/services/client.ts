b5b51d68a3bb5c389ea1561c4cc7f8460d63e408
octobrekids <chnrd.k@gmail.com>
2021-02-05 06:11:29 +0700

    add uri

--- src/common/services/client.ts
+++ src/common/services/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: process.env.APP_GRAPHQL_ENDPOINT,
+  uri: process.env.APP_GRAPHQL_ENDPOINT || 'http://localhost:5000/graphql',
   cache: new InMemoryCache(),
 });
