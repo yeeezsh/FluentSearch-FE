41dab1e97e93a77210d645f993e062545f9a9675
octobrekids <chnrd.k@gmail.com>
2021-02-05 01:34:21 +0700

    add uploadProgess style

---
+++ src/modules/upload/components/UploadProgress/styled.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background-color: white;
+  width: 400px;
+  overflow: scroll;
+  max-height: 400px;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+
+  h3 {
+    background-color: beige;
+    margin: 0;
+    padding: 20px;
+  }
+`;
