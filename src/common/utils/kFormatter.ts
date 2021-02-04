6370dcc076008192c1fc59e6584414d04720b0f7
octobrekids <chnrd.k@gmail.com>
2021-02-04 17:43:41 +0700

    change output type to string

--- src/common/utils/kFormatter.ts
+++ src/common/utils/kFormatter.ts
@@ -1,4 +1,4 @@
-const kFormatter = (num: number): string | number => {
-  return num > 999 ? (num / 1000).toFixed(1) + 'k' : num;
+const kFormatter = (num: number): string => {
+  return num > 999 ? (num / 1000).toFixed(1) + 'k' : num.toString();
 };
 export default kFormatter;
