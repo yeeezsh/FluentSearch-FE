fe3f53b1331298bf866e16393f191a54756a7fc5
octobrekids <chnrd.k@gmail.com>
2021-02-04 17:38:50 +0700

    fix test

--- src/common/tests/pages/dashboard.spec.tsx
+++ src/common/tests/pages/dashboard.spec.tsx
@@ -1,31 +1,47 @@
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import DashboardPage from 'Modules/dashboard/pages';
-import AlbumPreview from 'Modules/dashboard/components/AlbumPreview/index';
 import NumberCard from 'Modules/dashboard/components/DashboardCard/NumberCard/index';
 import ProgressCard from 'Modules/dashboard/components/DashboardCard/ProgressCard/index';
-import ModelCard from 'Modules/dashboard/components/DashboardCard/ModelCard/index';
+import dashboardReducer from 'Modules/dashboard/reducer/dashboardReducer';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const rootReducer = combineReducers({
+  dashboard: dashboardReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 describe('Pages/Dashboard test', () => {
-  it('Render correctly', () => {
-    const wrap = shallow(<DashboardPage />);
-    expect(wrap.html()).toMatchSnapshot();
-  });
-
   it('Dashboard should be exisiting', () => {
-    const wrap = shallow(<DashboardPage />);
+    const wrap = mount(
+      <Provider store={store}>
+        <DashboardPage />
+      </Provider>,
+    );
     expect(wrap.exists()).toBe(true);
   });
 
-  it('Should 4 Overview Album', () => {
-    const wrap = mount(<DashboardPage />);
-    expect(wrap.find(AlbumPreview)).toHaveLength(4);
+  it('Should have Overview Album', () => {
+    const wrap = mount(
+      <Provider store={store}>
+        <DashboardPage />
+      </Provider>,
+    );
+    expect(wrap.exists('OverviewAlbum')).toBe(true);
   });
 
   it('Should contain 2 NumberCard, 1 Progress Card and 1 Model Card', () => {
-    const wrap = mount(<DashboardPage />);
+    const wrap = mount(
+      <Provider store={store}>
+        <DashboardPage />
+      </Provider>,
+    );
     expect(wrap.find(NumberCard)).toHaveLength(2);
     expect(wrap.find(ProgressCard)).toHaveLength(1);
-    expect(wrap.find(ModelCard)).toHaveLength(1);
+    expect(wrap.find('ModelCard')).toHaveLength(1);
   });
 });
