077b1725b5e8589750a7a73745c5ec01e03fd65f
octobrekids <chnrd.k@gmail.com>
2021-02-05 01:29:41 +0700

    add header config and formdata

--- src/modules/upload/services/upload.photo.ts
+++ src/modules/upload/services/upload.photo.ts
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
 //TODO: Change to correct type
-export const uploadPhoto = async (formPayload: any): Promise<any> => {
-  const response = await axios({
-    baseURL: 'https://localhost:5000',
-    url: '/file',
-    method: 'post',
-    data: formPayload,
-  });
+export const uploadPhoto = async (owner: string, formPayload: any): Promise<any> => {
+  const files = new FormData();
+  files.append('file', formPayload);
+  const url = 'http://localhost:5000/file';
+  const config = {
+    headers: {
+      'content-type':
+        'multipart/form-data; boundary=----WebKitFormBoundaryqTqJIxvkWFYqvP5s',
+    },
+  };
+  const data = {
+    owner: owner,
+    files: files,
+  };
+  const response = await axios.post(url, data, config);
   return response.data;
 };
