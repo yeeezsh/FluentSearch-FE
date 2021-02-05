580fef6add56eda61bde5187c7a19d993c99655e
yee2542 <yee2542gtl@gmail.com>
2021-02-05 23:11:18 +0700

    remove unused code

--- src/modules/dashboard/services/fetch.dashboardData.ts
+++ src/modules/dashboard/services/fetch.dashboardData.ts
@@ -1,16 +1,6 @@
-// import axios from 'axios';
 import { AlbumPreviewProps } from '../components/AlbumPreview/types';
 import { AlbumPreviewMockData, DashboardMockData } from '../mocks/data';
 import { DashboardData } from '../models/types';
-
-// export const fetchDashboard = async (): Promise<DashboardData> => {
-//   const apiRoot = 'https://api.unsplash.com';
-//   const accessKey = 'fLLHNmXzlY1Mkc9woN0pQFNNc53hoBfGAgmQTF2OH4w';
-//   const { data } = await axios.get(
-//     `${apiRoot}/photos/random?client_id=${accessKey}&count=5`,
-//   );
-//   return data;
-// };
 
 type DashboardDataType = {
   DashboardMockData: DashboardData;
