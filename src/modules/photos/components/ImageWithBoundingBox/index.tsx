949fb7f34870a1296081bb91af10904843beffc4
yee2542 <yee2542gtl@gmail.com>
2021-02-06 06:42:47 +0700

    refactor

--- src/modules/photos/components/ImageWithBoundingBox/index.tsx
+++ src/modules/photos/components/ImageWithBoundingBox/index.tsx
@@ -6,65 +6,48 @@ import { Image } from './styled';
 type ImageWithBoundingBoxType = {
   tags?: Tag[];
   src: string;
-  width: number;
-  height: number;
-};
-
-type RenderBoundingBoxType = {
-  tags: Tag[];
-  scaleX: number;
-  scaleY: number;
-};
-
-const RenderBoundingBox: React.FC<RenderBoundingBoxType> = (props) => {
-  const { tags, scaleX, scaleY } = props;
-
-  return (
-    <>
-      {tags.map((e, index) => {
-        <BoundingBox
-          key={index}
-          xMax={e.xMax / scaleX}
-          xMin={e.xMin / scaleX - 100}
-          yMax={e.yMax / scaleY}
-          yMin={e.yMin / scaleY + 110}
-          label={e.result}
-        />;
-      })}
-    </>
-  );
+  originalWidth: number;
+  originalHeight: number;
 };
 
 const ImageWithBoundingBox: React.FC<ImageWithBoundingBoxType> = (props) => {
   const ref = useRef<HTMLImageElement>(null);
-  const [imageHeight, setImageHeight] = useState(0);
-  const [imageWidth, setImageWidth] = useState(0);
+  const [canvasHeight, setCanvasHeight] = useState(0);
+  const [canvasWidth, setCanvasWidth] = useState(0);
 
   const handleImageLoad = (event) => {
-    setImageHeight(event.target.clientHeight);
-    setImageWidth(event.target.clientWidth);
+    setCanvasHeight(event.target.clientHeight);
+    setCanvasWidth(event.target.clientWidth);
   };
 
-  const { tags, src, width, height } = props;
+  const { tags, src, originalWidth, originalHeight } = props;
 
-  const scaleX = width / imageWidth;
-  const scaleY = height / imageHeight;
-  console.log(imageHeight, imageWidth);
-  console.log(width, height);
+  const scaleX = canvasWidth / originalWidth;
+  const scaleY = canvasHeight / originalHeight;
+
+  console.log('scaleX', scaleX);
+  console.log('scaleY', scaleY);
+  console.log('original');
+  console.log(originalWidth, originalHeight);
+  console.log('canvas');
+  console.log(canvasHeight, canvasWidth);
   console.log(scaleX, scaleY);
 
   return (
     <>
       {tags && (
         <>
-          <BoundingBox
-            xMax={tags[0].xMax / scaleX}
-            xMin={tags[0].xMin / scaleX - 100}
-            yMax={tags[0].yMax / scaleY}
-            yMin={tags[0].yMin / scaleY + 110}
-            label={tags[0].result}
-          />
-          ;{/* <RenderBoundingBox tags={tags} scaleX={scaleX} scaleY={scaleY} /> */}
+          {props.tags &&
+            props.tags?.map((originSize) => (
+              <BoundingBox
+                key={Math.random()}
+                xMin={originSize.xMin * scaleX}
+                xMax={originSize.xMax * scaleX}
+                yMin={originSize.yMin * scaleY}
+                yMax={originSize.yMax * scaleY}
+                label={originSize.result}
+              />
+            ))}
         </>
       )}
       ;
