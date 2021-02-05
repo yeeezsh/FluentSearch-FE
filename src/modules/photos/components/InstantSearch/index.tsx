f2f72ff7806a21b4b1b4c70b8ade0a6f36c9766e
yee2542 <yee2542gtl@gmail.com>
2021-02-06 03:40:13 +0700

    add instant search reducer

---
+++ src/modules/photos/components/InstantSearch/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InstantSearch = () => {
+  return <div></div>;
+};
+
+export default InstantSearch;
---
+++ src/modules/photos/reducers/instantSearchReducer/index.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { InstantSearchReducer, INSTANT_SEARCH } from './types';
+const initialState: InstantSearchReducer = {
+  autocomplete: [],
+  result: [],
+  word: '',
+  loading: false,
+};
+
+export const instantSearchSlice = createSlice({
+  name: INSTANT_SEARCH,
+  initialState,
+  reducers: {
+    onSearch(state, action: PayloadAction<{ word: string }>) {
+      state.word = action.payload.word;
+    },
+    onData(
+      state,
+      action: PayloadAction<{
+        autocomplete: InstantSearchReducer['autocomplete'];
+        data: InstantSearchReducer['result'];
+        loading: boolean;
+      }>,
+    ) {
+      const { loading, data, autocomplete } = action.payload;
+      state.loading = loading;
+      state.autocomplete = autocomplete;
+      state.result = data;
+    },
+  },
+});
+
+export default instantSearchSlice.reducer;
+
+export const instantSearchActions = instantSearchSlice.actions;
