a92217cc5044a62f65d617e39bd19db3038e210f
yee2542 <yee2542gtl@gmail.com>
2021-02-05 23:49:03 +0700

    remove unused code

--- src/modules/history/services/fetch.history.ts
+++ src/modules/history/services/fetch.history.ts
@@ -1,12 +1,4 @@
-// import axios from 'axios';
 import { DataSource, IDataSource } from '../mocks/data';
-// import { HistoryData } from '../pages/types';
-
-// export const fetchHistory = async (): Promise<HistoryData[]> => {
-//   const apiRoot = 'https://6019ba397db53900178346f2.mockapi.io/History';
-//   const { data } = await axios.get(apiRoot);
-//   return data;
-// };
 
 export const fetchHistory = (): IDataSource[] => {
   return DataSource;
