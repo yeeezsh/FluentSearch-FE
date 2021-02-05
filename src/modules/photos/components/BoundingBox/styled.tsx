cbcbcfadf98454170a966306a9c00f10845e3c12
octobrekids <chnrd.k@gmail.com>
2021-02-06 02:36:25 +0700

    change bounding box style

--- src/modules/photos/components/BoundingBox/styled.tsx
+++ src/modules/photos/components/BoundingBox/styled.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 export const SquareStyle = styled.div`
-  max-height: inherit;
-  max-width: inherit;
   border: 3px solid red;
   background-color: transparent;
   z-index: 1000;
-  position: absolute;
+  position: fixed;
 `;
