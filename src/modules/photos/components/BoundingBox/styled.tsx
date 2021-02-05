6a3703443c2ee9961426fb47c7634110d8ce34d7
octobrekids <chnrd.k@gmail.com>
2021-02-06 05:56:10 +0700

    add Label

--- src/modules/photos/components/BoundingBox/styled.tsx
+++ src/modules/photos/components/BoundingBox/styled.tsx
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
 export const SquareStyle = styled.div`
-  border: 3px solid red;
+  border: 6px solid #5a36cc;
   background-color: transparent;
   position: absolute;
   display: flex;
   flex-wrap: wrap;
   width: 100%;
   height: 100%;
+`;
+
+export const Label = styled.div`
+  background-color: #5a36cc;
+  padding: 5px;
+  position: absolute;
+  color: white;
+  top: 100%;
+  right: -6%;
 `;
