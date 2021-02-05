2b76f0d4f20bbe899b35a0213d546642549329a4
yee2542 <yee2542gtl@gmail.com>
2021-02-05 22:38:39 +0700

    add todo

--- src/modules/photos/components/LoadedImage/index.tsx
+++ src/modules/photos/components/LoadedImage/index.tsx
@@ -1,6 +1,7 @@
 import { ImageProps } from '../Layouts/types';
 import { Img } from './styled';
 
+// TODO: not use ?
 export const LoadedImage = ({ url, key }: ImageProps): JSX.Element => {
   return <Img src={url} key={key} alt="" />;
 };
