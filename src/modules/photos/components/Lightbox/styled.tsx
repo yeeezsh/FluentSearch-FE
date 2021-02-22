d45a4bfca675f13fd7b83c1a71a9372fa6d7811b
octobrekids <chnrd.k@gmail.com>
2021-02-23 02:33:40 +0700

    init lightbox style component

---
+++ src/modules/photos/components/Lightbox/styled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const LightboxWrapper = styled.div``;
+
+const LightboxCard = styled.div``;
+
+const DetailCard = styled.div``;
+
+const OptionWrapper = styled.div``;
+
+const ButtonWrapper = styled.div``;
+
+const ImageWrapper = styled.div``;
