ae298b4031d201ab0f2f4be0adfa9ae3fa5aa797
octobrekids <chnrd.k@gmail.com>
2021-02-24 03:07:01 +0700

    move boundingbox type

--- src/modules/photos/components/BoundingBox/index.tsx
+++ src/modules/photos/components/BoundingBox/index.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import { SquareStyle, Label } from './styled';
-
-export type BoundingBoxType = {
-  xMin: number;
-  xMax: number;
-  yMin: number;
-  yMax: number;
-  label: string;
-  scaleBorder: number;
-  width: number;
-  height: number;
-};
+import { BoundingBoxType } from './types';
 
 const BoundingBox: React.FC<BoundingBoxType> = (props) => {
   const { xMin, xMax, yMin, yMax, label, width, height, scaleBorder } = props;
---
+++ src/modules/photos/components/BoundingBox/types.ts
@@ -0,0 +1,10 @@
+export type BoundingBoxType = {
+  xMin: number;
+  xMax: number;
+  yMin: number;
+  yMax: number;
+  label: string;
+  scaleBorder: number;
+  width: number;
+  height: number;
+};
