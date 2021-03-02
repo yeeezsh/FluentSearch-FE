20de88c01cb9c142433eb5005bea0355eebcda01
octobrekids <chnrd.k@gmail.com>
2021-03-02 21:29:06 +0700

    [WIP] add pause run and delete button

--- src/modules/task/components/TaskProgressBar/index.tsx
+++ src/modules/task/components/TaskProgressBar/index.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import { Caption, ProgressBar, TaskProgressBarWrapper } from './styled';
+import { CloseOutlined, PauseOutlined, CaretRightOutlined } from '@ant-design/icons';
+import { PausePlayButton, StopButton } from './styled';
 
 type TaskProgressBarType = {
   progress: number;
   elaspedTime: Date;
   inprogressPhoto: number;
   totalPhoto: number;
+  active: boolean;
 };
 
 const TaskProgressBar: React.FC<TaskProgressBarType> = (props) => {
-  const { progress, elaspedTime, inprogressPhoto, totalPhoto } = props;
+  const { progress, elaspedTime, inprogressPhoto, totalPhoto, active } = props;
   return (
     <TaskProgressBarWrapper>
-      <Caption>elasped time {dayjs(elaspedTime).format('HH:mm:ss')} </Caption>
-      <ProgressBar percent={progress} />
-      <Caption>
-        {inprogressPhoto} of {totalPhoto} photos
-      </Caption>
+      <div style={{ display: 'inline-block' }}>
+        <Caption>elasped time {dayjs(elaspedTime).format('HH:mm:ss')} </Caption>
+        <ProgressBar percent={progress} />
+        <Caption>
+          {inprogressPhoto} of {totalPhoto} photos
+        </Caption>
+      </div>
+      <div style={{ display: 'inline-block', height: '100%' }}>
+        <PausePlayButton active={active}>
+          {active ? <PauseOutlined /> : <CaretRightOutlined />}
+        </PausePlayButton>
+        <StopButton>
+          <CloseOutlined />
+        </StopButton>
+      </div>
     </TaskProgressBarWrapper>
   );
 };
--- src/modules/task/components/TaskProgressBar/styled.tsx
+++ src/modules/task/components/TaskProgressBar/styled.tsx
@@ -1,5 +1,9 @@
 import { Progress } from 'antd';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface IPausePlayButton {
+  active: boolean;
+}
 
 export const ProgressBar = styled(Progress)`
   .ant-progress-inner {
@@ -8,7 +12,7 @@ export const ProgressBar = styled(Progress)`
 
   .ant-progress-success-bg,
   .ant-progress-bg {
-    background-color: #5a36cc;
+    background-color: var(--secondary-color);
   }
 `;
 
@@ -19,4 +23,38 @@ export const Caption = styled.p`
 
 export const TaskProgressBarWrapper = styled.div`
   text-align: center;
+  align-items: center;
+  display: inline-block;
+`;
+
+export const PausePlayButton = styled.button<IPausePlayButton>`
+  text-decoration: none;
+  background-color: var(--secondary-color);
+  color: white;
+  max-height: 30px;
+  display: inline-block;
+  max-width: 30px;
+  padding: 0.25rem 1rem 1rem 0.5rem;
+  border-radius: 100%;
+  border: none;
+  ${(props) =>
+    props.active &&
+    css`
+      display: inline-block;
+      text-decoration: none;
+      padding: 0.5rem 1rem 0.5rem 1rem;
+      max-height: 30px;
+      max-width: 30px;
+      border-radius: 100%;
+      background-color: var(--green-color);
+      border: none;
+    `};
+`;
+
+export const StopButton = styled.button`
+  display: inline-block;
+  text-decoration: none;
+  color: var(--red-color);
+  background-color: transparent;
+  border: none;
 `;
