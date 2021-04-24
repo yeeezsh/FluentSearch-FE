d6add04fc71b6da3ab2b81592f85bb8ab00b0fb9
octobrekids <chnrd.k@gmail.com>
2021-04-24 23:22:14 +0700

    add TopControl

--- src/modules/videos/components/PlayerControl/index.tsx
+++ src/modules/videos/components/PlayerControl/index.tsx
@@ -1,10 +1,30 @@
+import { Row, Col } from 'antd';
 import React, { forwardRef } from 'react';
 import { ControlWrapper } from './styled';
 import { PlayerControlPropsType } from './types';
 
 const PlayerControl = forwardRef<HTMLDivElement, PlayerControlPropsType>((props, ref) => {
   const { isPlaying, muted, onPlaying, volume, playbackRate, played } = props;
-  return <ControlWrapper></ControlWrapper>;
+
+  const TopControl = () => (
+    <Row justify="space-between" style={{ padding: 16 }}>
+      <Col>
+        <h4 style={{ color: 'white' }}>Video Title</h4>
+      </Col>
+      <Col>bookmark</Col>
+    </Row>
+  );
+
+  return (
+    <ControlWrapper ref={ref}>
+      <TopControl />
+      <br />
+      middlecontrol
+      <br />
+      botttomocontrol
+      <br />
+    </ControlWrapper>
+  );
 });
 
 PlayerControl.displayName = 'PlayerControl';
