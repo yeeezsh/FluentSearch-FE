1f4363455cb508c09de3437de6628b6cce8e3a9e
octobrekids <chnrd.k@gmail.com>
2021-04-24 20:59:27 +0700

    add VideoStateType

---
+++ src/modules/videos/containers/VideoPlayerContainer/types.ts
@@ -0,0 +1,11 @@
+type VideoStateType = {
+  muted: boolean;
+  playing: boolean;
+  volume: number;
+  playbackRate: string;
+  seeking: boolean;
+  duration: number;
+  played: number;
+};
+
+export type { VideoStateType };
