0e9dbf6a4d86ec517ddb3b0ef2befedaffee2801
octobrekids <chnrd.k@gmail.com>
2021-04-24 22:06:59 +0700

    add handleProgress

--- src/modules/videos/containers/VideoPlayerContainer/index.tsx
+++ src/modules/videos/containers/VideoPlayerContainer/index.tsx
@@ -1,19 +1,44 @@
-import React, { useState } from 'react';
+import VideoPlayer from 'Modules/videos/components/VideoPlayer';
+import React, { useRef, useState } from 'react';
+import ReactPlayer from 'react-player';
 import { VideoPlayerWrapper } from './styled';
 import { VideoPlayerStateType } from './types';
 
-const VideoPlayerContainer = () => {
+const VideoPlayerContainer: React.FC = () => {
   const [videoPlayerState, setVideoPlayerState] = useState<VideoPlayerStateType>({
     muted: false,
-    playing: false,
+    isPlaying: false,
     volume: 50 / 100,
     playbackRate: '1.0',
-    seeking: false,
+    isSeeking: false,
     duration: 0,
     played: 0,
   });
 
-  return <VideoPlayerWrapper></VideoPlayerWrapper>;
+  const playerRef = useRef<ReactPlayer>(null);
+
+  const handleProgress = () => {
+    console.log('onProgress', videoPlayerState.played);
+    if (!videoPlayerState.isSeeking)
+      setVideoPlayerState((prevState) => ({
+        ...prevState,
+        played: videoPlayerState.played,
+      }));
+  };
+
+  return (
+    <VideoPlayerWrapper>
+      <VideoPlayer
+        url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        ref={playerRef}
+        isPlaying={videoPlayerState.isPlaying}
+        muted={videoPlayerState.muted}
+        playbackRate={videoPlayerState.playbackRate}
+        handleProgress={handleProgress}
+        volume={videoPlayerState.volume}
+      />
+    </VideoPlayerWrapper>
+  );
 };
 
 export default VideoPlayerContainer;
