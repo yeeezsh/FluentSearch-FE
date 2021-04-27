ead831501ad2ae8d70d5a8605893838c6f59529f
octobrekids <chnrd.k@gmail.com>
2021-04-28 02:32:21 +0700

    [wip] add video details card

--- src/modules/videos/pages/index.tsx
+++ src/modules/videos/pages/index.tsx
@@ -1,21 +1,45 @@
-import { Row, Col } from 'antd';
+import { Row, Col, Card } from 'antd';
 import LayoutWithSearch from 'Components/Layouts/LayoutWithSearch';
 import React from 'react';
 import { PageWrapper } from 'Styles/global';
+
+const PeopleCard = () => (
+  <Card title="N People">
+    <p>Card content</p>
+    <p>Card content</p>
+    <p>Card content</p>
+  </Card>
+);
+
+const LabelCard = () => (
+  <Card title="N Labels">
+    <p>Card content</p>
+    <p>Card content</p>
+    <p>Card content</p>
+  </Card>
+);
+
+const DetailCard = () => (
+  <Card title="Details">
+    <p>Card content</p>
+    <p>Card content</p>
+    <p>Card content</p>
+  </Card>
+);
 
 const ViewVideoPage: React.FC = () => {
   return (
     <LayoutWithSearch border={false}>
       <PageWrapper>
-        {'<'} Photos <br />
+        Photos <br />
         video-wedding <br />
         <hr />
-        <Row>
-          <Col>video with marker</Col>
-          <Col>
-            people <br />
-            labels <br />
-            details <br />
+        <Row justify="space-between">
+          <Col span={12}>video with marker</Col>
+          <Col span={10}>
+            <PeopleCard /> <br />
+            <LabelCard /> <br />
+            <DetailCard /> <br />
           </Col>
         </Row>
       </PageWrapper>
