70cb89a110a80a592669b50bb5ea426999d8b2fe
octobrekids <chnrd.k@gmail.com>
2021-04-29 17:48:37 +0700

    change avatar border color

--- src/modules/videos/components/Avatar/styled.tsx
+++ src/modules/videos/components/Avatar/styled.tsx
@@ -1,11 +1,12 @@
+import { Avatar } from 'antd';
 import styled from 'styled-components';
 import { AvatarWrapperProps } from './types';
 
-export const AvatarWrapper = styled.img<AvatarWrapperProps>`
+export const AvatarWrapper = styled(Avatar)<AvatarWrapperProps>`
   border-radius: 50%;
   ${(props) =>
     props.selected &&
     `
-    border: 3px solid red;
+    border: 3px solid #0BB5C2;
   `}
 `;
