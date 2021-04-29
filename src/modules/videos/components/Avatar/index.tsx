81a50e2b8b3e12aedb67bf1a88fba960ecc4d0da
octobrekids <chnrd.k@gmail.com>
2021-04-29 17:48:59 +0700

    add avatar size

--- src/modules/videos/components/Avatar/index.tsx
+++ src/modules/videos/components/Avatar/index.tsx
@@ -4,7 +4,14 @@ import { AvatarPropsType } from './types';
 
 const Avatar: React.FC<AvatarPropsType> = (props) => {
   const { selected = false, src } = props;
-  return <AvatarWrapper src={src} alt="" selected={selected} />;
+  return (
+    <AvatarWrapper
+      size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+      src={src}
+      alt=""
+      selected={selected}
+    />
+  );
 };
 
 export default Avatar;
