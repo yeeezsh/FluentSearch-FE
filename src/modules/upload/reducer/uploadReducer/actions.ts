22525fd6913e4ef7bbd97598a178273eea613363
octobrekids <chnrd.k@gmail.com>
2021-05-05 04:56:39 +0700

    add upload file data action

--- src/modules/upload/reducer/uploadReducer/actions.ts
+++ src/modules/upload/reducer/uploadReducer/actions.ts
@@ -1,8 +1,24 @@
-import { FileUpload } from 'Modules/upload/model/types';
+import { uploadFile } from 'Modules/upload/services/upload.file';
 import { store } from 'Stores/index';
+import { uploadActions } from '.';
 
-export const uploadFile = async (group: string, type: FileUpload['type']) => {
+export const uploadFileData = async (group: string): Promise<void> => {
   const pendingQueue = store
     .getState()
     .upload.pendingQueue.filter((f) => f.group === group);
+
+  for (const task of pendingQueue) {
+    const id = task._id;
+    const pendingQueueFilterID = store
+      .getState()
+      .upload.pendingQueue.filter((f) => f._id !== id);
+    store.dispatch(uploadActions.setPendingQueue(pendingQueueFilterID));
+
+    try {
+      await uploadFile(task.file);
+      store.dispatch(uploadActions.successUploadFile(task));
+    } catch (error) {
+      store.dispatch(uploadActions.failureUploadFile(task));
+    }
+  }
 };
