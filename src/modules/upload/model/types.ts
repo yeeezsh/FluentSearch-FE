b0441d1f951e1f9b731fdee17edb37dacb97c6f4
octobrekids <chnrd.k@gmail.com>
2021-05-05 04:07:03 +0700

    add file upload type

--- src/modules/upload/model/types.ts
+++ src/modules/upload/model/types.ts
@@ -1,1 +1,12 @@
 export const UPLOAD = 'UPLOAD';
+
+type FileUpload = {
+  _id: string;
+  file: FormData;
+  progress: number;
+  originFilename: string;
+  createAt: Date;
+  type: 'single' | 'multiple';
+  group: string;
+  state: 'waiting' | 'queue' | 'failed' | 'finish' | 'cancel';
+};
