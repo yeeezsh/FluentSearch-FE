286ccc6495cf51fab547d3c61445e9500e3259d0
yee2542 <yee2542gtl@gmail.com>
2021-05-04 23:29:52 +0700

    wip afterware

--- src/common/services/client.ts
+++ src/common/services/client.ts
@@ -1,4 +1,8 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, HttpLink, InMemoryCache } from '@apollo/client';
+
+const endpoint = new HttpLink({
+  uri: process.env.APP_GRAPHQL_ENDPOINT || 'http://localhost:5000/graphql',
+});
 
 export const client = new ApolloClient({
   uri: process.env.APP_GRAPHQL_ENDPOINT || 'http://localhost:5000/graphql',
