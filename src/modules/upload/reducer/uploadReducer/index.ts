3ef775f442de1fb639e81f1dd93c93dde5cbcf24
octobrekids <chnrd.k@gmail.com>
2021-05-05 04:19:52 +0700

    change init state

--- src/modules/upload/reducer/uploadReducer/index.ts
+++ src/modules/upload/reducer/uploadReducer/index.ts
@@ -1,47 +1,47 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { UPLOAD } from 'Modules/upload/model/types';
-import { modifyFiles } from 'Modules/upload/utils/uploadFile.utils';
-import { initUploadDataState } from './init';
-
-interface UploadProgress {
-  id: string;
-  progress: number;
-}
+import { initUploadState } from './init';
 
 export const uploadReducer = createSlice({
   name: UPLOAD,
-  initialState: initUploadDataState,
+  initialState: initUploadState,
   reducers: {
     init(state) {
-      return { ...state, ...initUploadDataState };
+      return { ...state, ...initUploadState };
     },
-    setUploadFile(state, action) {
-      // TODO: wtf this modifyFiles
-      const newData = modifyFiles(state.data.fileProgress, action.payload);
-      const existData = state.data.fileProgress;
-      const mergeData = [...existData, ...newData];
-      state.data.fileProgress = mergeData;
-    },
-    setUploadProgress(state, action: PayloadAction<UploadProgress>) {
-      const { id, progress } = action.payload;
-      const file = state.data.fileProgress.find((el, i) => el[i].id === id);
-      state.data.fileProgress[file?._id || ''].progress = progress;
-    },
-    successUploadFile(state, action: PayloadAction<UploadProgress>) {
-      const { id } = action.payload;
-      const file = state.data.fileProgress.find((el, i) => el[i].id === id);
-      state.data.fileProgress[file?._id || ''].status = 1;
-    },
-    failureUploadFile(state, action) {
-      const { id } = action.payload;
-      const file = state.data.fileProgress.find((el, i) => el[i].id === id);
-      state.data.fileProgress[file?._id || ''].status = 0;
-      state.data.fileProgress[file?._id || ''].progress = 0;
-    },
-    requestToUpload(state, action) {
-      const { url } = action.payload;
-      state.uploadUrl = url;
-    },
+    // setUploadFile(state, action: PayloadAction<FileList>) {
+    //   const mappedData: fileProgressType[] = [];
+    //   Array.from(action.payload).forEach((file, index) =>
+    //     mappedData.push({
+    //       id: index,
+    //       file: file,
+    //       progress: 0,
+    //       status: 0,
+    //     }),
+    //   );
+    //   const appendedData = [...state.data.fileProgress, ...mappedData];
+    //   state.data.fileProgress = appendedData;
+    // },
+    // setUploadProgress(state, action: PayloadAction<UploadProgress>) {
+    //   const { id, progress } = action.payload;
+    //   const file = state.data.fileProgress.find((el, i) => el[i].id === id);
+    //   state.data.fileProgress[file?._id || ''].progress = progress;
+    // },
+    // successUploadFile(state, action: PayloadAction<UploadProgress>) {
+    //   const { id } = action.payload;
+    //   const file = state.data.fileProgress.find((el, i) => el[i].id === id);
+    //   state.data.fileProgress[file?._id || ''].status = 1;
+    // },
+    // failureUploadFile(state, action) {
+    //   const { id } = action.payload;
+    //   const file = state.data.fileProgress.find((el, i) => el[i].id === id);
+    //   state.data.fileProgress[file?._id || ''].status = 0;
+    //   state.data.fileProgress[file?._id || ''].progress = 0;
+    // },
+    // requestToUpload(state, action) {
+    //   const { url } = action.payload;
+    //   state.uploadUrl = url;
+    // },
   },
 });
 export default uploadReducer.reducer;
