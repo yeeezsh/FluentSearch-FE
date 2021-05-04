d76bfa0c14fbf28bbbda337f18169f56f965e4ac
octobrekids <chnrd.k@gmail.com>
2021-05-05 05:34:46 +0700

    remove unused code

--- src/modules/upload/reducer/uploadReducer/index.ts
+++ src/modules/upload/reducer/uploadReducer/index.ts
@@ -27,39 +27,6 @@ export const uploadReducer = createSlice({
         state: 'failed',
       });
     },
-    // setUploadFile(state, action: PayloadAction<FileList>) {
-    //   const mappedData: fileProgressType[] = [];
-    //   Array.from(action.payload).forEach((file, index) =>
-    //     mappedData.push({
-    //       id: index,
-    //       file: file,
-    //       progress: 0,
-    //       status: 0,
-    //     }),
-    //   );
-    //   const appendedData = [...state.data.fileProgress, ...mappedData];
-    //   state.data.fileProgress = appendedData;
-    // },
-    // setUploadProgress(state, action: PayloadAction<UploadProgress>) {
-    //   const { id, progress } = action.payload;
-    //   const file = state.data.fileProgress.find((el, i) => el[i].id === id);
-    //   state.data.fileProgress[file?._id || ''].progress = progress;
-    // },
-    // successUploadFile(state, action: PayloadAction<UploadProgress>) {
-    //   const { id } = action.payload;
-    //   const file = state.data.fileProgress.find((el, i) => el[i].id === id);
-    //   state.data.fileProgress[file?._id || ''].status = 1;
-    // },
-    // failureUploadFile(state, action) {
-    //   const { id } = action.payload;
-    //   const file = state.data.fileProgress.find((el, i) => el[i].id === id);
-    //   state.data.fileProgress[file?._id || ''].status = 0;
-    //   state.data.fileProgress[file?._id || ''].progress = 0;
-    // },
-    // requestToUpload(state, action) {
-    //   const { url } = action.payload;
-    //   state.uploadUrl = url;
-    // },
   },
   extraReducers: (builder) => {
     builder.addCase(
