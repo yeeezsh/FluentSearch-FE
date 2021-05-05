dd466f5305e4ce51cae866e601e6461941a79856
octobrekids <chnrd.k@gmail.com>
2021-05-05 07:24:38 +0700

    update variable upload progress

--- src/modules/upload/components/UploadProgress/index.tsx
+++ src/modules/upload/components/UploadProgress/index.tsx
@@ -1,36 +1,22 @@
-import React, { useEffect } from 'react';
-import { size } from 'lodash';
+import React from 'react';
 import UploadItem from './UploadItem';
-import { useDispatch, useSelector } from 'react-redux';
-import { uploadPhotoData } from 'Modules/upload/reducer/uploadReducer/actions';
+import { useSelector } from 'react-redux';
 import { StoresState } from 'Stores/index';
 import { Wrapper } from './styled';
 
 const UploadProgress: React.FC = () => {
-  const dispatch = useDispatch();
-  const fileProgress = useSelector(
-    (state: StoresState) => state.upload.data.fileProgress,
-  );
-  const uploadedFileAmount = size(fileProgress);
+  const group = useSelector((state: StoresState) => state.upload.present.group);
+  const total = useSelector((state: StoresState) => state.upload.present.total);
 
-  useEffect(() => {
-    // TODO: if empty not try to upload
-    const fileToUpload = Object.values(fileProgress).filter(
-      (file) => file.progress === 0,
-    );
-    dispatch(uploadPhotoData(fileToUpload));
-  }, [uploadedFileAmount]);
-
-  return uploadedFileAmount > 0 ? (
+  return total > 0 ? (
     <Wrapper>
       <h4>Uploading File</h4>
-      {/* //TODO: wtf */}
-      {size(fileProgress) > 0
-        ? Object.values(fileProgress).map((file) => {
+      {total > 0
+        ? Object.values(group).map((file) => {
             return (
               <UploadItem
-                key={file._id}
-                file={{ file: file.file, progress: file.progress }}
+                key={Math.random()}
+                file={{ file: file, progress: file.progress }}
               />
             );
           })
