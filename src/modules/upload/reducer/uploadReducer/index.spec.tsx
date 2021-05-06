65de7c45119701b7e8c5f10159871ad644b37b7d
octobrekids <chnrd.k@gmail.com>
2021-05-07 06:01:04 +0700

    add set pending queue test

--- src/modules/upload/reducer/uploadReducer/index.spec.tsx
+++ src/modules/upload/reducer/uploadReducer/index.spec.tsx
@@ -1,4 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { FileUpload } from 'Modules/upload/model/types';
 import uploadReducer, { uploadActions } from '.';
 import { initUploadState } from './init';
 
@@ -10,10 +11,28 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+const mockFile: FileUpload[] = [
+  {
+    _id: '1',
+    progress: 0,
+    originFilename: 'test.jpg',
+    createAt: new Date(),
+    type: 'single',
+    group: 'groupnametest',
+    state: 'waiting',
+  },
+];
+
 describe('uploadReducer test', () => {
   it('it should correctly define initial state', () => {
     store.dispatch(uploadActions.init());
     const result = store.getState().upload;
     expect(result).toEqual(initUploadState);
+  });
+
+  it('it should set pending queue correctly', () => {
+    store.dispatch(uploadActions.setPendingQueue(mockFile));
+    const result = store.getState().upload.pendingQueue;
+    expect(result).toEqual(mockFile);
   });
 });
