805f2ad0269ff333a14b56997915cd544defa53c
octobrekids <chnrd.k@gmail.com>
2021-05-07 04:37:06 +0700

    fixed typo

--- src/modules/upload/components/SelectFileButton/index.tsx
+++ src/modules/upload/components/SelectFileButton/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { UploadBtnWrapper } from './styled';
+import { Wrapper } from './styled';
 import { PictureOutlined } from '@ant-design/icons';
 import { SelectFileButtonPropsType } from './type';
 
 const SelectFileButton: React.FC<SelectFileButtonPropsType> = (props) => {
   const { onChange } = props;
   return (
-    <UploadBtnWrapper>
+    <Wrapper>
       <input type="file" name="file" id="file" multiple onChange={onChange} />
       <label htmlFor="file">
         {/* //TODO: wtf &nbsp */}
         + &nbsp; &nbsp; &nbsp;
         <PictureOutlined /> &nbsp; &nbsp; Select Photos
       </label>
-    </UploadBtnWrapper>
+    </Wrapper>
   );
 };
 
--- src/modules/upload/components/SelectFileButton/styled.tsx
+++ src/modules/upload/components/SelectFileButton/styled.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const UploadBtnWrapper = styled.div`
+export const Wrapper = styled.div`
   input[type='file'] {
     width: 0.1px;
     height: 0.1px;
