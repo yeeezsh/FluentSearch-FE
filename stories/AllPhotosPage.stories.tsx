5a78e65eb8507884cb166f4a6ca1d8f668cd6994
octobrekids <chnrd.k@gmail.com>
2021-05-09 00:40:37 +0700

    fix page stories not work properly

--- stories/AllPhotosPage.stories.tsx
+++ stories/AllPhotosPage.stories.tsx
@@ -1,5 +1,10 @@
+import { ApolloProvider } from '@apollo/client';
 import { Meta } from '@storybook/react';
 import AllPhotosPage from 'Modules/photos/pages/index';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { client } from 'Services/client';
+import { store } from 'Stores/index';
 import { GlobalStyle } from 'Styles/global';
 
 export default {
@@ -10,6 +15,10 @@ export default {
 export const AllPhotoStories = (): JSX.Element => (
   <>
     <GlobalStyle />
-    <AllPhotosPage />
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <AllPhotosPage />
+      </ApolloProvider>
+    </Provider>
   </>
 );
