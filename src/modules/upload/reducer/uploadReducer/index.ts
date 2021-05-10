201bab3f74413aef0e816707e90b834456513a8c
octobrekids <chnrd.k@gmail.com>
2021-05-11 05:22:13 +0700

    remove extrareducer

--- src/modules/upload/reducer/uploadReducer/index.ts
+++ src/modules/upload/reducer/uploadReducer/index.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { FileUpload, GroupTask, UPLOAD } from 'Modules/upload/model/types';
-import { requestURLToUpload } from './actions';
 import { initUploadState } from './init';
 
 export const uploadReducer = createSlice({
@@ -35,14 +34,6 @@ export const uploadReducer = createSlice({
         state: 'failed',
       });
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(
-      requestURLToUpload.fulfilled,
-      (state, action: PayloadAction<{ data: string }>) => {
-        state.url = action.payload.data;
-      },
-    );
   },
 });
 export default uploadReducer.reducer;
