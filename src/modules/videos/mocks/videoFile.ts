fb2aa3db46b2bb5e573b01f3a5ae05bcb91f0040
octobrekids <chnrd.k@gmail.com>
2021-05-12 06:22:08 +0700

    [wip] add video file type

---
+++ src/modules/videos/mocks/videoFile.ts
@@ -0,0 +1,12 @@
+import { VideoFileType } from '../models/types';
+
+export const videoFile: VideoFileType = {
+  originalFileName: 'Jujutsu Kaisen',
+  delete: false,
+  size: 26530018,
+  type: 'undefined',
+  format: 'undefined',
+  width: 1200,
+  height: 800,
+  incidents: [],
+};
