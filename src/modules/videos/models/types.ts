28fd07bc66862596628ec4b0bd99acc8a5cd8b3c
octobrekids <chnrd.k@gmail.com>
2021-05-12 05:58:57 +0700

    add mediaType and mediaFormat

--- src/modules/videos/models/types.ts
+++ src/modules/videos/models/types.ts
@@ -18,4 +18,15 @@ type ProgressState = {
 
 type PlaybackRate = '0.5' | '0.75' | '1.0' | '1.5' | '2.0';
 
+enum MediaType {
+  VIDEO = 'video',
+  IMAGE = 'image',
+}
+
+enum MediaFormat {
+  MP4 = '.mp4',
+  JPG = '.jpg',
+}
+
 export type { VideoState, ProgressState, PlaybackRate };
+export { MediaType, MediaFormat };
