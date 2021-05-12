3eb539711b260687fc10990c0741310e6b7250fb
octobrekids <chnrd.k@gmail.com>
2021-05-13 02:01:12 +0700

    add fetchInsightData test

--- src/modules/videos/reducers/insightReducer/index.spec.tsx
+++ src/modules/videos/reducers/insightReducer/index.spec.tsx
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { annotation } from 'Modules/videos/mocks/annotation';
 import { combineReducers } from 'redux';
 import insightReducer, { insightActions } from '.';
+import { fetchInsightData } from './actions';
 import { initInsightState } from './init';
 
 const rootReducer = combineReducers({
@@ -20,5 +22,23 @@ describe('reducer/insightReducer test', () => {
     store.dispatch(insightActions.init());
     const result = store.getState().insight;
     expect(result).toEqual(initInsightState);
+  });
+
+  it('it should fetchInsightData correctly ', () => {
+    store.dispatch({
+      type: fetchInsightData.fulfilled.type,
+      payload: { data: annotation },
+    });
+
+    const expectedResultLabel = annotation.filter((el) => el.label !== 'person');
+    const expectedResultPerson = annotation.filter((el) => el.label === 'person');
+
+    const result = store.getState().insight.incidentData;
+    const resultPerson = store.getState().insight.present.person;
+    const resultLabel = store.getState().insight.present.label;
+
+    expect(result).toEqual(annotation);
+    expect(resultPerson).toEqual(expectedResultPerson);
+    expect(resultLabel).toEqual(expectedResultLabel);
   });
 });
