cc442d08b487d6035b48b780acd71425b70ae007
octobrekids <chnrd.k@gmail.com>
2021-05-13 01:52:54 +0700

    add fetchInsightData to reducer

--- src/modules/videos/reducers/insightReducer/index.ts
+++ src/modules/videos/reducers/insightReducer/index.ts
@@ -1,4 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AnnotaionType } from 'Modules/videos/models/types';
+import { ErrorStateCodeEnum } from 'Stores/common/types/error';
+import { fetchInsightData } from './actions';
 import { initInsightState } from './init';
 import { INSIGHT } from './type';
 
@@ -9,6 +12,28 @@ const insightSlice = createSlice({
     init(state) {
       return { ...state, ...initInsightState };
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchInsightData.pending, (state) => {
+      state.ready = false;
+      state.error = undefined;
+    });
+    builder.addCase(fetchInsightData.rejected, (state) => {
+      state.ready = false;
+      state.error = {
+        code: ErrorStateCodeEnum.ServerInternalError,
+        msg: 'api error',
+      };
+    });
+    builder.addCase(
+      fetchInsightData.fulfilled,
+      (state, action: PayloadAction<{ data: AnnotaionType[] }>) => {
+        const { data } = action.payload;
+        state.ready = true;
+        state.error = undefined;
+        state.incidentData = data;
+      },
+    );
   },
 });
 export default insightSlice.reducer;
