32a070a9b05736830d03d43c8f10ab0658d96a70
octobrekids <chnrd.k@gmail.com>
2021-05-13 01:45:48 +0700

    add fetchInsight

---
+++ src/modules/videos/services/fetch.insight.ts
@@ -0,0 +1,7 @@
+import { annotation } from '../mocks/annotation';
+import { AnnotaionType } from '../models/types';
+
+export const fetchInsight = (): AnnotaionType[] => {
+  //TODO: Change to fetch insight with keys
+  return annotation;
+};
