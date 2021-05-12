0d02a26217feddcbafff759bf64f84bb398826ef
octobrekids <chnrd.k@gmail.com>
2021-05-13 01:44:54 +0700

    remove unused code

--- src/modules/videos/reducers/videoReducer/index.ts
+++ src/modules/videos/reducers/videoReducer/index.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { database } from 'faker';
 import { MetaDataType, PlaybackRate, VideoFileType } from 'Modules/videos/models/types';
 import { ErrorStateCodeEnum } from 'Stores/common/types/error';
 import { fetchVideoData } from './actions';
