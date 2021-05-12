5a35ea865e6aa9f927dae7ca6423354c593d72f7
octobrekids <chnrd.k@gmail.com>
2021-05-12 07:30:20 +0700

     add error and ready to initVideoState type

--- src/modules/videos/reducers/videoReducer/init.ts
+++ src/modules/videos/reducers/videoReducer/init.ts
@@ -38,4 +38,5 @@ export const initVideoState: initVideoStateType = {
       fullscreen: false,
     },
   },
+  ready: false,
 };
--- src/modules/videos/reducers/videoReducer/types.ts
+++ src/modules/videos/reducers/videoReducer/types.ts
@@ -4,6 +4,7 @@ import {
   VideoFileType,
   VideoState,
 } from 'Modules/videos/models/types';
+import { ErrorState } from 'Stores/common/types/error';
 
 const VIDEO = 'VIDEO';
 
@@ -15,6 +16,8 @@ type initVideoStateType = {
     label: AnnotaionType[];
     player: VideoState;
   };
+  ready: boolean;
+  error?: ErrorState;
 };
 
 export { VIDEO };
