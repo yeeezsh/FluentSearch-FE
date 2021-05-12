ceb6f8e5c1a18fd96379a8ff926d25a9d63e8f4d
octobrekids <chnrd.k@gmail.com>
2021-05-12 07:12:46 +0700

    [wip] add fetch video services

---
+++ src/modules/videos/services/fetch.video.ts
@@ -0,0 +1,7 @@
+import { videoFile } from '../mocks/videoFile';
+import { VideoFileType } from '../models/types';
+
+export const fetchVideo = (): VideoFileType => {
+  //TODO: Change to fetch video file and url
+  return videoFile;
+};
