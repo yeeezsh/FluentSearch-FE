de94ea5aec0072bf7ababd6cf18d615ce3c45b32
octobrekids <chnrd.k@gmail.com>
2021-05-16 01:38:20 +0700

    add TaskData type

--- src/modules/task/reducer/taskReducer/types.ts
+++ src/modules/task/reducer/taskReducer/types.ts
@@ -3,20 +3,20 @@ import { ErrorState } from 'Stores/common/types/error';
 
 export const TASK = 'TASK';
 
+export type TaskData = {
+  key: string;
+  timestamp: string;
+  taskID: string;
+  taskName: string;
+  model: ModelEnum;
+  progress: number;
+  elaspedTime: string;
+  inprogressPhoto: number;
+  totalPhoto: number;
+};
+
 export type TaskState = {
-  data: [
-    {
-      key: string;
-      timestamp: string;
-      taskID: string;
-      taskName: string;
-      model: ModelEnum;
-      progress: number;
-      elaspedTime: string;
-      inprogressPhoto: number;
-      totalPhoto: number;
-    },
-  ][];
+  data: TaskData[];
   ready: boolean;
   error?: ErrorState;
 };
