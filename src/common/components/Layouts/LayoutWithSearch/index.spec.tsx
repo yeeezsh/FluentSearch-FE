f4a6fb4f1c5a3f92abc8256fafb9511c28d0eab8
octobrekids <chnrd.k@gmail.com>
2021-05-16 22:11:58 +0700

    update layout with search snapshot

--- src/common/components/Layouts/LayoutWithSearch/index.spec.tsx
+++ src/common/components/Layouts/LayoutWithSearch/index.spec.tsx
@@ -2,10 +2,28 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import LayoutWithSearch from '.';
 import { ContentWithPadding } from './styled';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { store } from 'Stores/index';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('Component/LayoutWithSearch test', () => {
+  const mocks = [];
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('Should match snapshots', () => {
-    const wrap = shallow(<LayoutWithSearch />);
+    const wrap = renderer
+      .create(
+        <Provider store={store}>
+          <MockedProvider mocks={mocks}>
+            <LayoutWithSearch />
+          </MockedProvider>
+        </Provider>,
+      )
+      .toJSON();
     expect(wrap).toMatchSnapshot();
   });
 
