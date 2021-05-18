f225050a22666e90fd2d830779987969c1c34bb9
octobrekids <chnrd.k@gmail.com>
2021-05-18 21:38:59 +0700

    fixed marker position

--- src/modules/videos/components/TimeWithMarker/Marker/styled.tsx
+++ src/modules/videos/components/TimeWithMarker/Marker/styled.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 export const MarkerWrapper = styled.i`
+  position: absolute;
   background-color: #5a36cc;
   cursor: pointer;
   display: block;
   height: 1.5rem;
-  width: 4px;
-  top: 9px;
+  width: 5px;
+  top: 0px;
   left: 50%;
 `;
