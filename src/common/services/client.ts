e9d72aaf1cbe805de19af2c451172428ba1adb03
octobrekids <chnrd.k@gmail.com>
2021-05-19 06:13:36 +0700

    add handle duplicate email

--- src/common/services/client.ts
+++ src/common/services/client.ts
@@ -1,6 +1,43 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, ServerError } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import HttpStatusCode from '../constants/httpStatusCode';
+
+export const END_POINT =
+  process.env.APP_GRAPHQL_ENDPOINT || 'http://api.fluentsearch.ml/graphql';
+const httpLink = new HttpLink({
+  uri: END_POINT,
+  credentials: 'include',
+});
+
+const logoutLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    for (const err of graphQLErrors) {
+      switch (err.extensions && err.extensions.exception.response.statusCode) {
+        case 400:
+          alert('This email already exist, Please change the email');
+          break;
+        default:
+          console.log(
+            `[GraphQL error]: Status: ${err.extensions?.exception.response.statusCode} Message: ${err.message}, Location: ${err.locations}, Path: ${err.path}`,
+          );
+          break;
+      }
+    }
+  }
+
+  if (networkError) {
+    if ((networkError as ServerError)?.statusCode === HttpStatusCode.UNAUTHORIZED)
+      window.location.replace('/logout');
+    else if (
+      (networkError as ServerError)?.statusCode === HttpStatusCode.NOT_IMPLEMENTED
+    ) {
+      alert(networkError?.message);
+      window.location.replace('/');
+    }
+  }
+});
 
 export const client = new ApolloClient({
-  uri: process.env.APP_GRAPHQL_ENDPOINT || 'http://localhost:5000/graphql',
+  link: logoutLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
