bb5192f44e772a9b5abd82b40da5b0c1a0818072
octobrekids <chnrd.k@gmail.com>
2021-05-19 07:17:20 +0700

    change init user state and type

--- src/modules/user/reducers/userReducer/init.ts
+++ src/modules/user/reducers/userReducer/init.ts
@@ -1,8 +1,30 @@
+import {
+  UserRoleEnum,
+  UserPackageEnum,
+  UserZoneEnum,
+} from '../../../../common/generated/generated-types';
 import { UserState } from './types';
 
 export const initUserState: UserState = {
-  id: '',
-  username: '',
-  displayName: '',
+  user: {
+    _id: '',
+    createDate: '',
+    deactivate: false,
+    email: [],
+    mainEmail: '',
+    name: '',
+    package: UserPackageEnum.FreeUser,
+    role: UserRoleEnum.User,
+    updateDate: '',
+    zone: UserZoneEnum.Th1,
+  },
+  present: {
+    id: '',
+    username: '',
+    displayName: '',
+    role: UserRoleEnum.User,
+    zone: UserZoneEnum.Th1,
+    package: UserPackageEnum.FreeUser,
+  },
   authenticated: false,
 };
--- src/modules/user/reducers/userReducer/types.ts
+++ src/modules/user/reducers/userReducer/types.ts
@@ -1,9 +1,23 @@
+import {
+  UserPackageEnum,
+  UserRoleEnum,
+  UserWithId,
+  UserZoneEnum,
+} from '../../../../common/generated/generated-types';
+
 export const USER = 'USER';
 
+export type User = Omit<UserWithId, 'oauth' | 'password'>;
 export type UserState = {
-  id: string;
-  username: string;
-  displayName: string;
+  user: User;
+  present: {
+    id: string;
+    username: string;
+    displayName: string;
+    role: UserRoleEnum;
+    zone: UserZoneEnum;
+    package: UserPackageEnum;
+  };
   authenticated: boolean;
   msg?: string;
 };
