35cc3ab863919c098cf166722d48b6100f7001df
octobrekids <chnrd.k@gmail.com>
2021-05-19 13:16:33 +0700

    change sample video and annotation data

--- src/modules/videos/mocks/videoFile.ts
+++ src/modules/videos/mocks/videoFile.ts
@@ -7,8 +7,8 @@ export const videoFile: VideoFileType = {
   size: 26530018,
   type: 'undefined',
   format: '.jpg',
-  width: 960,
-  height: 540,
+  width: 1920,
+  height: 1080,
   date: 'Wed May 12 2021 23:06:42 GMT+0700 (Indochina Time)',
   place: 'Jujutsu Academy',
 };
