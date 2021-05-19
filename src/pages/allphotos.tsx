327e741210547e883d844ecbe93687471aeb2f5e
octobrekids <chnrd.k@gmail.com>
2021-05-20 00:29:40 +0700

    add RouterGuard to page

--- src/pages/allphotos.tsx
+++ src/pages/allphotos.tsx
@@ -1,8 +1,13 @@
 import AllPhotosPage from 'Modules/photos/pages';
 import React from 'react';
+import RouterGuard from '../common/components/RouterGuard';
 
 const AllPhotos: React.FC = () => {
-  return <AllPhotosPage />;
+  return (
+    <RouterGuard>
+      <AllPhotosPage />
+    </RouterGuard>
+  );
 };
 
 export default AllPhotos;
--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,8 +1,13 @@
 import LogoutPage from 'Modules/user/pages/login';
 import React from 'react';
+import RouterGuard from '../common/components/RouterGuard';
 
 const Logout: React.FC = () => {
-  return <LogoutPage />;
+  return (
+    <RouterGuard>
+      <LogoutPage />
+    </RouterGuard>
+  );
 };
 
 export default Logout;
