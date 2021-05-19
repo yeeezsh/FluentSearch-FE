fb836aad955b43313a7a59069cbcc22a32680979
octobrekids <33366852+octobrekids@users.noreply.github.com>
2021-05-20 03:38:26 +0700

    Add user reducer (#55)
    * add userReducer
    
    * add user service
    
    * add msg to type
    
    * add message error
    
    * add  await to data
    
    * remove duplicate function
    
    * add dispatch requestToLogin
    
    * fix typo
    
    * change service name
    
    * add register thunk
    
    * add adapter interceptors authen
    
    * unauthorized dispatch delete user
    
    * remove unused code
    
    * add handle onsubmit login
    
    * [wip] onfinish login
    
    * add dependency fluentsearch-types
    
    * add httpStatusCode
    
    * change schema path
    
    * remove unused code
    
    * add name property to type
    
    * add graphql query and mutation codegen
    
    * remove unused code
    
    * remove credential data
    
    * add handle duplicate email
    
    * update snapshot test
    
    * remove unused code
    
    * add dependency react-test-renderer with types
    
    * add createUser logic and handling
    
    * change init user state and type
    
    * fix setUser
    
    * fix bad userReducer test
    
    * change user state  type property
    
    * bad unmatch type setUser
    
    * remove unused actions
    
    * [wip] fix login and register reducer test
    
    * add login handling
    
    * add handle invalid credential
    
    * remove unused code
    
    * add logout page
    
    * fix bad graphQLerror
    
    * add RouterGuard Component
    
    * add RouterGuard to page
    
    * fix logout bad redirect
    
    * remove unused code
    
    * fix bad graphQLError handling code
    
    * fix bad login spec
    
    * fix bad register spec
    
    * fix duplicate login and register spec code
    
    * fix bad security hotspot

--- src/common/services/client.ts
+++ src/common/services/client.ts
@@ -1,6 +1,52 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, from, HttpLink, InMemoryCache, ServerError } from '@apollo/client';
+import { ErrorResponse, onError } from '@apollo/client/link/error';
+import HttpStatusCode from '../constants/httpStatusCode';
+
+export const END_POINT =
+  process.env.APP_GRAPHQL_ENDPOINT || 'https://api.fluentsearch.ml/graphql';
+const httpLink = new HttpLink({
+  uri: END_POINT,
+  credentials: 'include',
+});
+
+const logoutLink = onError(({ graphQLErrors, networkError }: ErrorResponse) => {
+  if (graphQLErrors) {
+    console.log(graphQLErrors[0]);
+    if (graphQLErrors[0].extensions) {
+      switch (graphQLErrors[0].extensions?.code) {
+        case 'GRAPHQL_VALIDATION_FAILED':
+          alert('GRAPHQL_VALIDATION_FAILED');
+          break;
+        case 'INTERNAL_SERVER_ERROR':
+          alert('Invalid email or password');
+          break;
+        case 'UNAUTHENICATED':
+          //TODO: need to implement refresh token
+          alert('UNAUTHENICATED');
+          window.location.replace('/login');
+          break;
+        default:
+          console.log(
+            `[GraphQL error]: Message: ${graphQLErrors[0].message}, Location: ${graphQLErrors[0].locations}, Path: ${graphQLErrors[0].path}`,
+          );
+          break;
+      }
+    }
+  }
+
+  if (networkError) {
+    if ((networkError as ServerError)?.statusCode === HttpStatusCode.UNAUTHORIZED)
+      window.location.replace('/logout');
+    else if (
+      (networkError as ServerError)?.statusCode === HttpStatusCode.NOT_IMPLEMENTED
+    ) {
+      alert(networkError?.message);
+      window.location.replace('/');
+    }
+  }
+});
 
 export const client = new ApolloClient({
-  uri: process.env.APP_GRAPHQL_ENDPOINT || 'http://localhost:5000/graphql',
+  link: from([logoutLink, httpLink]),
   cache: new InMemoryCache(),
 });
