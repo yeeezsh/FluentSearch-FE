d3f5d9ff91fd5557c0b8cecd35cc9ce3dfdc35d2
octobrekids <chnrd.k@gmail.com>
2021-05-19 07:17:41 +0700

    fix setUser

--- src/modules/user/reducers/userReducer/index.ts
+++ src/modules/user/reducers/userReducer/index.ts
@@ -1,8 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { MSG_INTERNAL_ERROR } from 'Modules/user/services/default.msg';
-import { getUser, requestLogin } from './actions';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { initUserState } from './init';
-import { USER } from './types';
+import { User, USER } from './types';
 
 const userSlice = createSlice({
   name: USER,
@@ -11,21 +9,12 @@ const userSlice = createSlice({
     init(state) {
       return { ...state, ...initUserState };
     },
-    setUser(state, action) {
-      return {
-        ...state,
-        ...action.payload,
-      };
+    setUser(state, action: PayloadAction<User>) {
+      state.user = action.payload;
     },
     deleteUser(state) {
       return { ...state, ...initUserState };
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(requestLogin.rejected, (state) => {
-      state.authenticated = false;
-      state.msg = MSG_INTERNAL_ERROR;
-    });
   },
 });
 
