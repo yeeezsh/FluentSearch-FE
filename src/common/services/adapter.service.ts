14eb0989c7c66246aeae8b448ce21312ca6049d8
yee2542 <yee2542gtl@gmail.com>
2021-05-26 23:45:32 +0700

    hotfix storage endpoint

--- src/common/services/adapter.service.ts
+++ src/common/services/adapter.service.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const END_POINT =
-  process.env.APP_BACKEND_STORAGE_ENDPOINT || 'http://localhost:3000/';
+  process.env.APP_BACKEND_STORAGE_ENDPOINT || 'http://storage.fluentsearch.ml';
 
 class Adapter {
   instance: AxiosInstance;
