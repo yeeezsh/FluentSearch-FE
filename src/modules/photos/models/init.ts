9e333b98ac20fd207a8bcd7e1b47eea9b1f56f20
octobrekids <chnrd.k@gmail.com>
2021-05-29 14:35:36 +0700

    change init state

--- src/modules/photos/models/init.ts
+++ src/modules/photos/models/init.ts
@@ -1,36 +1,9 @@
-export const initialState = {
-  id: '',
-  created_at: '',
-  updated_at: '',
-  width: 0,
-  height: 0,
-  color: '',
-  blur_hash: '',
-  downloads: 0,
-  likes: 0,
-  liked_by_user: false,
-  description: '',
-  location: {
-    name: '',
-    titile: '',
-    city: '',
-    country: '',
-  },
-  urls: {
-    raw: '',
-    full: '',
-    regular: '',
-    small: '',
-    thumb: '',
-  },
-  links: {
-    self: '',
-    html: '',
-    download: '',
-    download_location: '',
-  },
-  user: {
-    name: '',
-    total_likes: 0,
-  },
+import { RecentFile } from '../../../common/generated/generated-types';
+
+export const initialState: RecentFile = {
+  createAt: '',
+  original_filename: '',
+  updateAt: '',
+  uri: '',
+  uri_thumbnail: '',
 };
