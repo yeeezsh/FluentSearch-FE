45ac082f94a4ddad0a7909dbea70e99cb28701a2
octobrekids <chnrd.k@gmail.com>
2021-05-31 03:34:12 +0700

    remove unused type

--- src/modules/photos/pages/types.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloError } from '@apollo/client';
-import { GetUserBySessionQuery } from '../../../common/generated/generated-types';
-
-export type AllPhotosPagePropsType = {
-  props: React.PropsWithChildren<{
-    data?: GetUserBySessionQuery | undefined;
-    error?: ApolloError | undefined;
-  }>;
-};
