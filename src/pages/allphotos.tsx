4eff18e2a275f7bd79354a83973be7cded3279ab
octobrekids <chnrd.k@gmail.com>
2021-05-31 03:54:21 +0700

    remove unused get recentfile get serversideprops

--- src/pages/allphotos.tsx
+++ src/pages/allphotos.tsx
@@ -2,42 +2,13 @@ import AllPhotosPage from 'Modules/photos/pages';
 import { GetServerSideProps } from 'next';
 import React from 'react';
 import RouterGuard from '../common/components/RouterGuard';
-import { PageGetRecentFilesComp, ssrGetRecentFiles } from '../common/generated/page';
 
-const AllPhotos: PageGetRecentFilesComp = (props) => {
+const AllPhotos = () => {
   return (
     <RouterGuard>
-      <AllPhotosPage props={props} />
+      <AllPhotosPage />
     </RouterGuard>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const user = localStorage.getItem('user');
-  let owner = '';
-  if (user) {
-    const userData = JSON.parse(user);
-    owner = userData._id;
-  }
-
-  const data = await ssrGetRecentFiles.getServerPage(
-    {
-      variables: {
-        owner: owner,
-      },
-      context: {
-        headers: {
-          cookie: ctx.req.headers.cookie,
-        },
-      },
-    },
-    ctx,
-  );
-  return {
-    props: {
-      data,
-    },
-  };
 };
 
 export default AllPhotos;
