940fbcf63fcfff5f2de2d414726a8f728a8fcdeb
octobrekids <chnrd.k@gmail.com>
2021-05-31 12:43:41 +0700

    hide progress card and model card

--- src/modules/dashboard/pages/index.tsx
+++ src/modules/dashboard/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'antd';
 import {
   DashboardWrapper,
@@ -11,15 +11,19 @@ import AlbumPreview from 'Modules/dashboard/components/AlbumPreview/index';
 import NumberCard from 'Modules/dashboard/components/DashboardCard/NumberCard/index';
 import ProgressCard from 'Modules/dashboard/components/DashboardCard/ProgressCard/index';
 import ModelCard from 'Modules/dashboard/components/DashboardCard/ModelCard/index';
-import { fetchDashboardData } from '../reducer/dashboardReducer/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { StoresState } from 'Stores/index';
-import { DashboardData } from '../models/types';
+import { Dashboard, FileInsight } from '../models/types';
 import { AlbumPreviewProps } from '../components/AlbumPreview/types';
 import LayoutWithSearch from 'Components/Layouts/LayoutWithSearch';
+import {
+  InsightModelEnum,
+  useGetDashboardDataQuery,
+} from '../../../common/generated/generated-types';
+import { initState } from '../constants/init';
 
 type dashboardCardType = {
-  data: DashboardData;
+  data: Dashboard;
 };
 
 type OverviewAlbumType = {
@@ -28,38 +32,32 @@ type OverviewAlbumType = {
 
 const DashboardCard: React.FC<dashboardCardType> = (props) => {
   const { data } = props;
+
   return (
     <CardWrapper>
-      <Row justify="space-around" align="middle">
-        <Col md={5}>
+      <Row justify="start">
+        <Col md={5} style={{ marginRight: '5%' }}>
           <NumberCard
             cardName={'Photos'}
-            largeNumber={data.todayVideos}
-            todayNumber={data.todayPhotos}
+            largeNumber={data.FileDashboardData?.photo.total}
+            todayNumber={data.FileDashboardData?.photo.today}
           />
         </Col>
         <Col md={5}>
           <NumberCard
             cardName={'Videos'}
-            largeNumber={data.totalPhotos}
-            todayNumber={data.todayVideos}
+            largeNumber={data.FileDashboardData?.video.total}
+            todayNumber={data.FileDashboardData?.video.today}
           />
+        </Col>
+
+        {/* TODO: Not implemented progress card and model card 
+        <Col md={5}>
+          <ProgressCard cardName={'Task'} progress={0} doneNumber={0} totalNumber={0} />
         </Col>
         <Col md={5}>
-          <ProgressCard
-            cardName={'Task'}
-            progress={data.progressPhoto}
-            doneNumber={data.finishRunningPhotos}
-            totalNumber={data.totalRunningPhotos}
-          />
-        </Col>
-        <Col md={5}>
-          <ModelCard
-            cardName={'Model'}
-            model={data.model}
-            largeNumber={data.processWithModelPhoto}
-          />
-        </Col>
+          <ModelCard cardName={'Model'} model={model} largeNumber={0} />
+        </Col> */}
       </Row>
     </CardWrapper>
   );
@@ -86,9 +84,50 @@ const OverviewAlbum: React.FC<OverviewAlbumType> = (props) => {
 
 const DashboardPage: React.FC = () => {
   const dispatch = useDispatch();
+  const owner = useSelector((store: StoresState) => store.user.user.id);
+  const { data, loading, error } = useGetDashboardDataQuery({
+    variables: {
+      owner: owner,
+    },
+  });
+
+  console.log(data);
+
+  const [dashboardData, setDashboardData] = useState<Dashboard>(initState);
+
+  const queryDataFileDashboard = {
+    photo: {
+      today: data?.GetFileDashboard.photo.today || 0,
+      total: data?.GetFileDashboard.photo.total || 0,
+    },
+    video: {
+      today: data?.GetFileDashboard.video.today || 0,
+      total: data?.GetFileDashboard.video.total || 0,
+    },
+  };
+
+  const queryDataFileInsightDashboard: FileInsight[] =
+    data?.GetRecentFileInsightDashboard.map((el) => ({
+      fileMeta: {
+        _id: el.fileMeta._id,
+        type: el.fileMeta.type,
+        uri: el.fileMeta.uri,
+        uri_thumbnail: el.fileMeta.uri_thumbnail,
+      },
+      insights:
+        el.insights?.map((insight) => ({
+          keyword: insight.keyword,
+          model: insight.model,
+        })) || [],
+    })) || [];
+
   useEffect(() => {
-    dispatch(fetchDashboardData());
-  }, []);
+    setDashboardData({
+      FileDashboardData: queryDataFileDashboard,
+      FileInsightDashboardData: queryDataFileInsightDashboard,
+    });
+    //dispatch(fetchDashboardData());
+  }, [loading]);
   const DashboardDataStore = useSelector(
     (state: StoresState) => state.dashboard.data.dashboardData,
   );
@@ -101,7 +140,7 @@ const DashboardPage: React.FC = () => {
       <DashboardWrapper>
         <ContentWrapper>
           <DashboardHeader>Dashboard</DashboardHeader>
-          <DashboardCard data={DashboardDataStore} />
+          <DashboardCard data={dashboardData} />
 
           <AlbumWrapper>
             {/*TODO: Lastest Album*/}
