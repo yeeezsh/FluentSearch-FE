ee54f8010224575d208dd26bc838b1a1ec333459
octobrekids <chnrd.k@gmail.com>
2021-05-31 09:20:33 +0700

    fix lightbox test

--- src/modules/photos/components/Lightbox/Lightbox.spec.tsx
+++ src/modules/photos/components/Lightbox/Lightbox.spec.tsx
@@ -1,14 +1,20 @@
+import { MockedProvider } from '@apollo/client/testing';
 import { mount } from 'enzyme';
 import React from 'react';
 import Lightbox from '.';
-import BoundingBox from '../BoundingBox';
 import { mock } from './constants';
 import { LightboxCardLeft, LightboxCardRight, Image } from './styled';
 
+const mocks = [];
 describe('Lightbox Components', () => {
   let wrap;
+
   beforeEach(() => {
-    wrap = mount(<Lightbox {...mock} />);
+    wrap = mount(
+      <MockedProvider mocks={mocks}>
+        <Lightbox {...mock} />
+      </MockedProvider>,
+    );
   });
   it('Should import correctly', () => {
     expect(wrap.exists()).toBe(true);
@@ -19,8 +25,7 @@ describe('Lightbox Components', () => {
     expect(wrap.find(LightboxCardRight).exists()).toBe(true);
   });
 
-  it('Should have Image and BoundingBox', () => {
-    expect(wrap.find(BoundingBox).exists()).toBe(true);
+  it('Should have Image', () => {
     expect(wrap.find(Image).exists()).toBe(true);
   });
 
@@ -29,9 +34,9 @@ describe('Lightbox Components', () => {
     expect(wrap.find('b').at(0).text()).toEqual(' Info');
     expect(wrap.find('b').at(1).text()).toEqual(' Tag');
     expect(wrap.find('b').at(2).text()).toEqual(' Details');
-    expect(wrap.find('b').at(3).text()).toEqual(' Date');
-    expect(wrap.find('b').at(4).text()).toEqual(' Photo');
-    expect(wrap.find('b').at(5).text()).toEqual(' Place');
+    expect(wrap.find('b').at(3).text()).toEqual(' Model');
+    expect(wrap.find('b').at(4).text()).toEqual(' Date');
+    expect(wrap.find('b').at(5).text()).toEqual(' Photo');
   });
 
   it('Should have properly props', () => {
--- src/modules/photos/components/Lightbox/constants.ts
+++ src/modules/photos/components/Lightbox/constants.ts
@@ -1,10 +1,19 @@
 import { LightboxPropsType } from './types';
-import { mockData } from 'Modules/photos/mocks/data';
 function mockFunction(): void {
   console.log('click');
 }
 export const mock: LightboxPropsType = {
-  image: mockData[0],
+  image: {
+    _id: '1',
+    original_filename: 'cat.jpg',
+    uri:
+      'https://aumento.officemate.co.th/media/catalog/product/h/t/httpss3-ap-southeast-1.amazonaws.compim-prod-product-images68bc68bcce438a2454140271526c7e2497bdce6813ad_mkp0539918dummy.jpg?imwidth=640',
+    uri_thumbnail:
+      'https://aumento.officemate.co.th/media/catalog/product/h/t/httpss3-ap-southeast-1.amazonaws.compim-prod-product-images68bc68bcce438a2454140271526c7e2497bdce6813ad_mkp0539918dummy.jpg?imwidth=640',
+    createAt: 'today',
+    updateAt: 'today',
+    type: 'image',
+  },
   onPrev: mockFunction,
   onNext: mockFunction,
   closeLightbox: mockFunction,
